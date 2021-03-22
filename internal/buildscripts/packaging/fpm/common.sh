5fd92a212ca7bb3d82bd58606d525610323e31df
Jeff Cheng <jeffreyc@splunk.com>
2021-03-22 16:43:39 -0400

    Include smart agent bundle in amd64 deb/rpm (#177)

--- internal/buildscripts/packaging/fpm/common.sh
+++ internal/buildscripts/packaging/fpm/common.sh
@@ -16,9 +16,15 @@ SERVICE_GROUP="splunk-otel-collector"
 
 OTELCOL_INSTALL_PATH="/usr/bin/otelcol"
 CONFIG_REPO_PATH="$REPO_DIR/cmd/otelcol/config/collector/agent_config.yaml"
-CONFIG_INSTALL_PATH="/etc/otel/collector/gateway_config.yaml"
+CONFIG_INSTALL_PATH="/etc/otel/collector/agent_config.yaml"
 SERVICE_REPO_PATH="$FPM_DIR/$SERVICE_NAME.service"
 SERVICE_INSTALL_PATH="/lib/systemd/system/$SERVICE_NAME.service"
+
+FLUENTD_CONFIG_INSTALL_DIR="/etc/otel/collector/fluentd"
+
+SMART_AGENT_RELEASE_URL="https://api.github.com/repos/signalfx/signalfx-agent/releases"
+BUNDLE_BASE_DIR="/usr/lib/splunk-otel-collector"
+AGENT_BUNDLE_INSTALL_DIR="$BUNDLE_BASE_DIR/agent-bundle"
 
 PREINSTALL_PATH="$FPM_DIR/preinstall.sh"
 POSTINSTALL_PATH="$FPM_DIR/postinstall.sh"
@@ -43,6 +49,38 @@ create_user_group() {
         sudo useradd --system --user-group --no-create-home --shell /sbin/nologin $SERVICE_USER
 }
 
+download_smart_agent() {
+    local tag="$1"
+    local buildroot="$2"
+    local api_url=""
+    local dl_url=""
+
+    if [ "$tag" = "latest" ]; then
+        tag=$( curl -sL "$SMART_AGENT_RELEASE_URL/latest" | jq -r '.tag_name' )
+        if [ -z "$tag" ]; then
+            echo "Failed to get tag_name for latest release from $SMART_AGENT_RELEASE_URL/latest" >&2
+            exit 1
+        fi
+    fi
+
+    api_url="$SMART_AGENT_RELEASE_URL/tags/$tag"
+    dl_url="$( curl -sL "$api_url" | jq -r '.assets[] .browser_download_url' | grep "signalfx-agent-${tag#v}\.tar\.gz" )"
+    if [ -z "$dl_url" ]; then
+        echo "Failed to get the agent download url from $api_url" >&2
+        exit 1
+    fi
+
+    echo "Downloading $dl_url ..."
+    curl -sL "$dl_url" -o "$buildroot/signalfx-agent.tar.gz"
+
+    mkdir -p "$buildroot/$BUNDLE_BASE_DIR"
+    tar -xzf "$buildroot/signalfx-agent.tar.gz" -C "$buildroot/$BUNDLE_BASE_DIR"
+    mv "$buildroot/$BUNDLE_BASE_DIR/signalfx-agent" "$buildroot/$AGENT_BUNDLE_INSTALL_DIR"
+    rm -f "$buildroot/$AGENT_BUNDLE_INSTALL_DIR/bin/signalfx-agent"
+    rm -f "$buildroot/$AGENT_BUNDLE_INSTALL_DIR/bin/agent-status"
+    rm -f "$buildroot/signalfx-agent.tar.gz"
+}
+
 setup_files_and_permissions() {
     local otelcol="$1"
     local buildroot="$2"
@@ -58,10 +96,15 @@ setup_files_and_permissions() {
     cp -f "$CONFIG_REPO_PATH" "$buildroot/$CONFIG_INSTALL_PATH"
     sudo chown -R $SERVICE_USER:$SERVICE_GROUP "$buildroot/etc/otel"
     sudo chmod -R 755 "$buildroot/etc/otel"
-    sudo chmod 600 "$buildroot/etc/otel/collector/splunk_env.example"
+    sudo chmod 600 "$buildroot/etc/otel/collector/$SERVICE_NAME.conf.example"
 
     mkdir -p "$buildroot/$(dirname $SERVICE_INSTALL_PATH)"
     cp -f "$SERVICE_REPO_PATH" "$buildroot/$SERVICE_INSTALL_PATH"
     sudo chown root:root "$buildroot/$SERVICE_INSTALL_PATH"
     sudo chmod 644 "$buildroot/$SERVICE_INSTALL_PATH"
+
+    if [ -d "$buildroot/$BUNDLE_BASE_DIR" ]; then
+        sudo chown -R $SERVICE_USER:$SERVICE_GROUP "$buildroot/$BUNDLE_BASE_DIR"
+        sudo chmod -R 755 "$buildroot/$BUNDLE_BASE_DIR"
+    fi
 }
--- internal/buildscripts/packaging/fpm/postinstall.sh
+++ internal/buildscripts/packaging/fpm/postinstall.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+if [ -f /usr/lib/splunk-otel-collector/agent-bundle/bin/patch-interpreter ]; then
+    /usr/lib/splunk-otel-collector/agent-bundle/bin/patch-interpreter /usr/lib/splunk-otel-collector/agent-bundle
+fi
+
 if command -v systemctl >/dev/null 2>&1; then
     systemctl daemon-reload
     systemctl enable splunk-otel-collector.service

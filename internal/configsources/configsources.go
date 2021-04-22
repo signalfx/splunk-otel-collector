3bf5e1225131f00673f4a74faac2e0e34799431b
Paulo Janotti <pjanotti@splunk.com>
2021-04-22 06:53:28 -0700

    Create default config sources (#319)

---
+++ internal/configsources/configsources.go
@@ -0,0 +1,29 @@
+// Copyright 2020 Splunk, Inc.
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package configsources lists all config sources that can be used in the configuration.
+package configsources
+
+import (
+	"github.com/signalfx/splunk-otel-collector/internal/configprovider"
+	"github.com/signalfx/splunk-otel-collector/internal/configsource/vaultconfigsource"
+)
+
+// Get returns the factories to all config sources available to the user.
+func Get() []configprovider.Factory {
+	return []configprovider.Factory{
+		vaultconfigsource.NewFactory(),
+	}
+}

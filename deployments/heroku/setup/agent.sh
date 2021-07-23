ab0dd4aab9418698f64d0d99bd41e2a30e110fde
Steve Flanders <sflanders@splunk.com>
2021-07-23 15:53:45 -0400

    Add patch-interpreter and copyright

--- deployments/heroku/setup/agent.sh
+++ deployments/heroku/setup/agent.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Copyright Splunk Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 if [ "$DYNOTYPE" == "run" ]; then
     exit 0
 fi
@@ -41,6 +55,8 @@ else
 fi
 
 # Start connector
+
+(cd $SPLUNK_CONFIG_DIR/signalfx-agent/ && bin/patch-interpreter $SPLUNK_CONFIG_DIR/signalfx-agent/)
 
 chmod a+x $SPLUNK_CONFIG_DIR/otelcol_linux_amd64
 $SPLUNK_CONFIG_DIR/otelcol_linux_amd64 > $SPLUNK_LOG_FILE 2>&1&

bfecb360a223399d44efd36811b39419858c9129
jeffreyc-splunk <jeffreyc@splunk.com>
2022-04-12 11:55:35 -0400

    Update installer test for auto instrumentation

--- internal/buildscripts/packaging/tests/installer_test.py
+++ internal/buildscripts/packaging/tests/installer_test.py
@@ -45,6 +45,8 @@ OLD_CONFIG_PATH = "/etc/otel/collector/splunk_config_linux.yaml"
 TOTAL_MEMORY = "256"
 BALLAST = "64"
 REALM = "test"
+INSTR_CONF_PATH = "/usr/lib/splunk-instrumentation/instrumentation.conf"
+LIBSPLUNK_PATH = "/usr/lib/splunk-instrumentation/libsplunk.so"
 
 
 def verify_env_file(container, mode="agent", ballast=None):
@@ -88,6 +90,16 @@ def verify_support_bundle(container):
     run_container_cmd(container, "test -f /tmp/splunk-support-bundle.tar.gz")
 
 
+def verify_uninstall(container, distro):
+    run_container_cmd(container, "sh -x /test/install.sh --uninstall")
+
+    for pkg in ("splunk-otel-collector", "td-agent", "splunk-otel-auto-instrumentation"):
+        if distro in DEB_DISTROS:
+            assert container.exec_run(f"dpkg -s {pkg}").exit_code != 0
+        else:
+            assert container.exec_run(f"rpm -q {pkg}").exit_code != 0
+
+
 @pytest.mark.installer
 @pytest.mark.parametrize(
     "distro",
@@ -115,6 +127,12 @@ def test_installer_mode(distro, version, mode):
             run_container_cmd(container, install_cmd, env={"VERIFY_ACCESS_TOKEN": "false"})
             time.sleep(5)
 
+            # verify splunk-otel-auto-instrumentation is not installed
+            if distro in DEB_DISTROS:
+                assert container.exec_run("dpkg -s splunk-otel-auto-instrumentation").exit_code != 0
+            else:
+                assert container.exec_run("rpm -q splunk-otel-auto-instrumentation").exit_code != 0
+
             # verify env file created with configured parameters
             verify_env_file(container, mode=mode)
 
@@ -127,7 +145,7 @@ def test_installer_mode(distro, version, mode):
             # test support bundle script
             verify_support_bundle(container)
 
-            run_container_cmd(container, "sh -x /test/install.sh --uninstall")
+            verify_uninstall(container, distro)
 
         finally:
             if "opensuse" not in distro:
@@ -172,7 +190,7 @@ def test_installer_ballast(distro, version):
             if "opensuse" not in distro:
                 assert container.exec_run("systemctl status td-agent").exit_code == 0
 
-            run_container_cmd(container, "sh -x /test/install.sh --uninstall")
+            verify_uninstall(container, distro)
 
         finally:
             if "opensuse" not in distro:
@@ -219,6 +237,8 @@ def test_installer_service_owner(distro, version):
             if "opensuse" not in distro:
                 assert container.exec_run("systemctl status td-agent").exit_code == 0
 
+            verify_uninstall(container, distro)
+
         finally:
             if "opensuse" not in distro:
                 run_container_cmd(container, "journalctl -u td-agent --no-pager")
@@ -262,7 +282,111 @@ def test_installer_without_fluentd(distro, version):
             else:
                 assert container.exec_run("rpm -q td-agent").exit_code != 0
 
-            run_container_cmd(container, "sh -x /test/install.sh --uninstall")
+            verify_uninstall(container, distro)
+
+        finally:
+            run_container_cmd(container, f"journalctl -u {SERVICE_NAME} --no-pager")
+
+
+@pytest.mark.installer
+@pytest.mark.instrumentation
+@pytest.mark.parametrize(
+    "distro",
+    [pytest.param(distro, marks=pytest.mark.deb) for distro in DEB_DISTROS]
+    + [pytest.param(distro, marks=pytest.mark.rpm) for distro in RPM_DISTROS],
+    )
+@pytest.mark.parametrize("version", VERSIONS)
+def test_installer_with_instrumentation(distro, version):
+    install_cmd = f"sh -x /test/install.sh -- testing123 --realm {REALM} --memory {TOTAL_MEMORY} --with-instrumentation"
+
+    if version != "latest":
+        install_cmd = f"{install_cmd} --collector-version {version.lstrip('v')}"
+
+    if STAGE != "release":
+        assert STAGE in ("test", "beta"), f"Unsupported stage '{STAGE}'!"
+        install_cmd = f"{install_cmd} --{STAGE}"
+
+    print(f"Testing installation on {distro} from {STAGE} stage ...")
+    with run_distro_container(distro) as container:
+        copy_file_into_container(container, INSTALLER_PATH, "/test/install.sh")
+
+        try:
+            # run installer script
+            run_container_cmd(container, install_cmd, env={"VERIFY_ACCESS_TOKEN": "false"})
+            time.sleep(5)
+
+            # verify env file created with configured parameters
+            verify_env_file(container)
+
+            # verify collector service status
+            assert wait_for(lambda: service_is_running(container, service_owner=SERVICE_OWNER))
+
+            # verify splunk-otel-auto-instrumentation is installed
+            if distro in DEB_DISTROS:
+                assert container.exec_run("dpkg -s splunk-otel-auto-instrumentation").exit_code == 0
+            else:
+                assert container.exec_run("rpm -q splunk-otel-auto-instrumentation").exit_code == 0
+
+            # verify /etc/ld.so.preload is configured
+            run_container_cmd(container, f"grep '^{LIBSPLUNK_PATH}$' /etc/ld.so.preload")
+
+            # verify deployment.environment attribute is not set
+            run_container_cmd(container, f"grep -v '^resource_attributes=deployment.environment=.*$' {INSTR_CONF_PATH}")
+
+            verify_uninstall(container, distro)
+
+        finally:
+            run_container_cmd(container, f"journalctl -u {SERVICE_NAME} --no-pager")
+
+
+@pytest.mark.installer
+@pytest.mark.instrumentation
+@pytest.mark.parametrize(
+    "distro",
+    [pytest.param(distro, marks=pytest.mark.deb) for distro in DEB_DISTROS]
+    + [pytest.param(distro, marks=pytest.mark.rpm) for distro in RPM_DISTROS],
+    )
+@pytest.mark.parametrize("version", VERSIONS)
+def test_installer_with_deployment_environment(distro, version):
+    install_cmd = f"sh -x /test/install.sh -- testing123 --realm {REALM} --memory {TOTAL_MEMORY} --with-instrumentation"
+
+    if version != "latest":
+        install_cmd = f"{install_cmd} --collector-version {version.lstrip('v')}"
+
+    if STAGE != "release":
+        assert STAGE in ("test", "beta"), f"Unsupported stage '{STAGE}'!"
+        install_cmd = f"{install_cmd} --{STAGE}"
+
+    install_cmd = f"{install_cmd} --deployment-environment test"
+
+    print(f"Testing installation on {distro} from {STAGE} stage ...")
+    with run_distro_container(distro) as container:
+        copy_file_into_container(container, INSTALLER_PATH, "/test/install.sh")
+
+        try:
+            # run installer script
+            run_container_cmd(container, install_cmd, env={"VERIFY_ACCESS_TOKEN": "false"})
+            time.sleep(5)
+
+            # verify env file created with configured parameters
+            verify_env_file(container)
+
+            # verify collector service status
+            assert wait_for(lambda: service_is_running(container, service_owner=SERVICE_OWNER))
+
+            # verify splunk-otel-auto-instrumentation is installed
+            if distro in DEB_DISTROS:
+                assert container.exec_run("dpkg -s splunk-otel-auto-instrumentation").exit_code == 0
+            else:
+                assert container.exec_run("rpm -q splunk-otel-auto-instrumentation").exit_code == 0
+
+            # verify /etc/ld.so.preload is configured
+            run_container_cmd(container, f"grep '^{LIBSPLUNK_PATH}$' /etc/ld.so.preload")
+
+            # verify deployment.environment is set
+            run_container_cmd(container, f"grep '^resource_attributes=deployment.environment=test$' {INSTR_CONF_PATH}")
+
+            verify_uninstall(container, distro)
 
         finally:
             run_container_cmd(container, f"journalctl -u {SERVICE_NAME} --no-pager")

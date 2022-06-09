bc7eb0a0f7327a5498539341cab81d6939f92c41
jeffreyc-splunk <jeffreyc@splunk.com>
2022-06-09 18:15:12 -0400

    Add auto instrumentation support to puppet

--- deployments/puppet/manifests/params.pp
+++ deployments/puppet/manifests/params.pp
@@ -21,6 +21,9 @@ class splunk_otel_collector::params {
     }
     $fluentd_config_source = "${fluentd_config_dir}/fluent.conf"
     $fluentd_config_dest = $fluentd_config_source
+    $auto_instrumentation_version = 'latest'
+    $auto_instrumentation_java_agent_jar = '/usr/lib/splunk-instrumentation/splunk-otel-javaagent.jar'
+
   } elsif $::osfamily == 'windows' {
     $collector_version = ''
     $collector_install_dir = "${::win_programfiles}\\Splunk\\OpenTelemetry Collector"

62500280370756ff887bd04d85e0650e205da347
Antoine Toulme <antoine@lunar-ocean.com>
2022-08-12 15:29:23 -0700

    simplify tar packaging

--- internal/buildscripts/packaging/fpm/tar/build.sh
+++ internal/buildscripts/packaging/fpm/tar/build.sh
@@ -23,6 +23,58 @@ VERSION="${1:-}"
 ARCH="${2:-amd64}"
 OUTPUT_DIR="${3:-$REPO_DIR/dist}"
 SMART_AGENT_RELEASE="${4:-}"
+BUNDLE_BASE_DIR=""
+AGENT_BUNDLE_INSTALL_DIR=""
+OTELCOL_INSTALL_PATH="/bin/otelcol"
+TRANSLATESFX_INSTALL_PATH="/bin/translatesfx"
+
+tar_download_smart_agent() {
+    local tag="$1"
+    local buildroot="$2"
+    local api_url=""
+    local dl_url=""
+
+    if [ "$tag" = "latest" ]; then
+        tag=$( curl -sL "$SMART_AGENT_RELEASE_URL/latest" | jq -r '.tag_name' )
+        if [ -z "$tag" ]; then
+            echo "Failed to get tag_name for latest release from $SMART_AGENT_RELEASE_URL/latest" >&2
+            exit 1
+        fi
+    fi
+
+    api_url="$SMART_AGENT_RELEASE_URL/tags/$tag"
+    dl_url="$( curl -sL "$api_url" | jq -r '.assets[] .browser_download_url' | grep "signalfx-agent-${tag#v}\.tar\.gz" )"
+    if [ -z "$dl_url" ]; then
+        echo "Failed to get the agent download url from $api_url" >&2
+        exit 1
+    fi
+
+    echo "Downloading $dl_url ..."
+    curl -sL "$dl_url" -o "$buildroot/signalfx-agent.tar.gz"
+
+    mkdir -p "$buildroot/$BUNDLE_BASE_DIR"
+    tar -xzf "$buildroot/signalfx-agent.tar.gz" -C "$buildroot/$BUNDLE_BASE_DIR"
+    find "$buildroot/$AGENT_BUNDLE_INSTALL_DIR" -wholename "*test*.key" -delete -or -wholename "*test*.pem" -delete
+    rm -f "$buildroot/signalfx-agent.tar.gz"
+}
+
+tar_setup_files_and_permissions() {
+    local otelcol="$1"
+    local translatesfx="$2"
+    local buildroot="$3"
+
+    create_user_group
+
+    mkdir -p "$buildroot/$(dirname $OTELCOL_INSTALL_PATH)"
+    cp -f "$otelcol" "$buildroot/$OTELCOL_INSTALL_PATH"
+    sudo chown root:root "$buildroot/$OTELCOL_INSTALL_PATH"
+    sudo chmod 755 "$buildroot/$OTELCOL_INSTALL_PATH"
+
+    mkdir -p "$buildroot/$(dirname $TRANSLATESFX_INSTALL_PATH)"
+    cp -f "$translatesfx" "$buildroot/$TRANSLATESFX_INSTALL_PATH"
+    sudo chown root:root "$buildroot/$TRANSLATESFX_INSTALL_PATH"
+    sudo chmod 755 "$buildroot/$TRANSLATESFX_INSTALL_PATH"
+}
 
 if [[ -z "$VERSION" ]]; then
     VERSION="$( get_version )"
@@ -39,10 +91,10 @@ translatesfx_path="$REPO_DIR/bin/translatesfx_linux_${ARCH}"
 buildroot="$(mktemp -d)"
 
 if [ "$ARCH" = "amd64" ]; then
-    download_smart_agent "$SMART_AGENT_RELEASE" "$buildroot"
+    tar_download_smart_agent "$SMART_AGENT_RELEASE" "$buildroot"
 fi
 
-setup_files_and_permissions "$otelcol_path" "$translatesfx_path" "$buildroot"
+tar_setup_files_and_permissions "$otelcol_path" "$translatesfx_path" "$buildroot"
 
 mkdir -p "$OUTPUT_DIR"
 
@@ -52,10 +104,6 @@ sudo fpm -s dir -t tar -n "${PKG_NAME}_${VERSION}_${ARCH}" -v "$VERSION" -f -p "
     --description "$PKG_DESCRIPTION" \
     --license "$PKG_LICENSE" \
     --url "$PKG_URL" \
-    --architecture "$ARCH" \
-    --config-files "$AGENT_CONFIG_INSTALL_PATH" \
-    --config-files "$GATEWAY_CONFIG_INSTALL_PATH" \
-    --config-files "$FLUENTD_CONFIG_INSTALL_DIR" \
     "$buildroot/"=/
 
 cd "$OUTPUT_DIR"

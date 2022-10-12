020da6a5f03ff2c65f45d137547abb2e841462f2
Antoine Toulme <atoulme@splunk.com>
2022-10-12 10:12:03 -0700

    Add additional fixes that are found by running lint for all modules

--- examples/prometheus-federation/prom-counter/main.go
+++ examples/prometheus-federation/prom-counter/main.go
@@ -1,3 +1,18 @@
+// Copyright OpenTelemetry Authors
+// Copyright Splunk, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
@@ -21,10 +36,14 @@ func initMeter() {
 	if err != nil {
 		log.Panicf("failed to initialize prometheus exporter %v", err)
 	}
-	http.HandleFunc("/", exporter.ServeHTTP)
-	go func() {
-		_ = http.ListenAndServe(":8080", nil)
-	}()
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", exporter.ServeHTTP)
+	server := &http.Server{
+		ReadHeaderTimeout: 5 * time.Second,
+		Handler:           mux,
+		Addr:              ":8080",
+	}
+	go server.ListenAndServe()
 }
 
 func main() {

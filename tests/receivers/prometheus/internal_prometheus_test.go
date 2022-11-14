42d3566a7db31467754bca73e42c2d25338139fa
Ryan Fitzpatrick <10867373+rmfitzpatrick@users.noreply.github.com>
2022-11-14 14:35:43 -0500

    Add internal prometheus metrics integration test (#2265)

---
+++ tests/receivers/prometheus/internal_prometheus_test.go
@@ -0,0 +1,30 @@
+// Copyright Splunk, Inc.
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//go:build integration
+
+package tests
+
+import (
+	"testing"
+
+	"github.com/signalfx/splunk-otel-collector/tests/testutils"
+)
+
+func TestInternalPrometheusMetrics(t *testing.T) {
+	testutils.AssertAllMetricsReceived(
+		t, "internal.yaml", "internal_metrics_config.yaml", nil,
+	)
+}

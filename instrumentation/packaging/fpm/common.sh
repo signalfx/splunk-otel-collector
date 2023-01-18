ee02a3ff5302c58479ab6ede5dee3f53439bc171
Jeff Cheng <83052155+jeffreyc-splunk@users.noreply.github.com>
2023-01-18 12:26:08 -0500

    Fix deb/rpm versioning in github workflows (#2486)
    * Use "fetch-depth: 0" to get tags for version
    
    * Revert hack
    
    * Update mount path and set safe.directory
    
    * Set safe.directory

--- instrumentation/packaging/fpm/common.sh
+++ instrumentation/packaging/fpm/common.sh
@@ -39,13 +39,11 @@ get_version() {
     commit_tag="$( git -C "$REPO_DIR" describe --abbrev=0 --tags --exact-match --match 'v[0-9]*' 2>/dev/null || true )"
     if [[ -z "$commit_tag" ]]; then
         latest_tag="$( git -C "$REPO_DIR" describe --abbrev=0 --match 'v[0-9]*' 2>/dev/null || true )"
-        # DIRTY DIRTY TRIAGE HACK THAT TODO NEEDS TO BE FIXED ASAP
-        echo "${latest_tag:-v0.67.0}-post"
-        #if [[ -n "$latest_tag" ]]; then
-        #    echo "${latest_tag}-post"
-        #else
-        #    echo "0.0.1-post"
-        #fi
+        if [[ -n "$latest_tag" ]]; then
+            echo "${latest_tag}-post"
+        else
+            echo "0.0.1-post"
+        fi
     else
         echo "$commit_tag"
     fi

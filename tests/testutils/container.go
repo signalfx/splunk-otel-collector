6a1b8023de1affab5741b43707413ed30edbcebe
Curtis Robert <crobert@splunk.com>
2023-02-24 10:53:41 -0800

    Fix reflection tests with new wait strategies

--- tests/testutils/container.go
+++ tests/testutils/container.go
@@ -232,7 +232,7 @@ func (container Container) Build() *Container {
 		NetworkMode:    networkMode,
 		Labels:         container.Labels,
 		Privileged:     container.Privileged,
-		WaitingFor:     wait.ForAll(container.WaitingFor...).WithStartupTimeoutDefault(startupTimeout),
+		WaitingFor:     wait.ForAll(container.WaitingFor...).WithDeadline(startupTimeout),
 	}
 	return &container
 }

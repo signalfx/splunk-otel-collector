f0dfe807eff7447456601b3f5114c3185ee53047
jeffreyc-splunk <jeffreyc@splunk.com>
2023-04-18 10:29:18 -0400

    Update windows bundled python to 3.8.16

--- pkg/signalfx-agent/bundle/scripts/windows/bundle.ps1
+++ pkg/signalfx-agent/bundle/scripts/windows/bundle.ps1
@@ -18,7 +18,7 @@ $repoDir = "$scriptDir\..\..\..\..\.."
 . $scriptDir\common.ps1
 
 $BUILD_DIR="$repoDir\build"
-$PYTHON_VERSION="3.8.10"
+$PYTHON_VERSION="3.8.16"
 $PIP_VERSION="21.0.1"
 $NUGET_URL="https://aka.ms/nugetclidl"
 $NUGET_EXE="nuget.exe"

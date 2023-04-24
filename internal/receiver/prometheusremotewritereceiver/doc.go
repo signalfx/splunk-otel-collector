a33de009e68559fd3c2486c2b9d427ae87d58cdf
james hughes <jameshughes@splunk.com>
2023-04-24 12:35:34 -0700

    properly regenerate metrics

--- internal/receiver/prometheusremotewritereceiver/doc.go
+++ internal/receiver/prometheusremotewritereceiver/doc.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build mdatgen
+//go:build mdatagen
 
 //go:generate mdatagen metadata.yaml
 

b39149cfd87923900b8c5f8c4e39f64856c76bb0
Antoine Toulme <antoine@lunar-ocean.com>
2023-05-26 16:14:03 -0700

    Remove use of File.exists?, prefer File.exist? instead (#3191)

--- deployments/puppet/lib/facter/local_users.rb
+++ deployments/puppet/lib/facter/local_users.rb
@@ -5,7 +5,7 @@ Facter.add(:local_users) do
   confine :kernel => 'Linux'
   setcode do
     users = Array.new
-    if File.exists?("/etc/passwd")
+    if File.exist?("/etc/passwd")
       File.open("/etc/passwd").each do |line|
         next if line.match(/^\s|^#|^$/)
         users << line.split(':').first

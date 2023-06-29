004f5da96218de887337a8e9a6952988130cba52
Ryan Fitzpatrick <10867373+rmfitzpatrick@users.noreply.github.com>
2023-06-29 16:15:28 -0400

    expand postgres discovery rules (#3329)

--- tests/receivers/smartagent/postgresql/testdata/server/initdb.d/init.sh
+++ tests/receivers/smartagent/postgresql/testdata/server/initdb.d/init.sh
@@ -9,4 +9,4 @@
 ##################################################################################################
 set -euo pipefail
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "test_db" -c "CREATE EXTENSION pg_stat_statements;"
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "test_db" -c "CREATE EXTENSION IF NOT EXISTS pg_stat_statements;"

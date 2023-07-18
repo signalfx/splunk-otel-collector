1b6dc57086b9b33cd15c9c37a1857be6c7b6e2de
Curtis Robert <crobert@splunk.com>
2023-07-18 15:24:49 -0700

    Change tmp dir's name

--- tests/receivers/jmxreceiver/jmxreceiver_test.go
+++ tests/receivers/jmxreceiver/jmxreceiver_test.go
@@ -40,7 +40,7 @@ func TestJMXReceiverProvidesAllJVMMetrics(t *testing.T) {
 			func(collector testutils.Collector) testutils.Collector {
 				// JMX requires a local directory that can be written to, so we must mount a local dir
 				// that the collector has write access to.
-				tmp_dir := "/etc/otel/collector/tmp"
+				tmp_dir := "/etc/otel/collector/testdata"
 				mount_dir, err := filepath.Abs(filepath.Join(".", "testdata"))
 				require.NoError(t, err)
 				return collector.WithEnv(map[string]string{

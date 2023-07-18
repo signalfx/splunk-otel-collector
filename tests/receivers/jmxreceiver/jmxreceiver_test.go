a9b978fc2b231c0de302c92fe8aa40fe81fc08c6
Curtis Robert <crobert@splunk.com>
2023-07-18 14:47:08 -0700

    Try debugging failure

--- tests/receivers/jmxreceiver/jmxreceiver_test.go
+++ tests/receivers/jmxreceiver/jmxreceiver_test.go
@@ -34,19 +34,18 @@ func TestJMXReceiverProvidesAllJVMMetrics(t *testing.T) {
 		).WithExposedPorts("7199:7199").WithName("jmx").WillWaitForPorts("7199"),
 	}
 
-	// JMX requires a local directory that can be written to, so we must mount a local dir
-	// that the collector has write access to.
-	tmp_dir := "/etc/otel/collector/tmp"
-	mount_dir, err := filepath.Abs(filepath.Join(".", "testdata"))
-	require.NoError(t, err)
-
 	testutils.AssertAllMetricsReceived(t, "all.yaml",
 		"all_metrics_config.yaml", containers,
 		[]testutils.CollectorBuilder{
 			func(collector testutils.Collector) testutils.Collector {
+				// JMX requires a local directory that can be written to, so we must mount a local dir
+				// that the collector has write access to.
+				tmp_dir := "/etc/otel/collector/tmp"
+				mount_dir, err := filepath.Abs(filepath.Join(".", "testdata"))
+				require.NoError(t, err)
 				return collector.WithEnv(map[string]string{
 					"TMPDIR": tmp_dir,
-				}).WithMount(mount_dir, tmp_dir)
+				}).WithMount(mount_dir, tmp_dir).WillFail(true)
 			},
 		})
 }

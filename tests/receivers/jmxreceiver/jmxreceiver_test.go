7d0be25ff65102ecc31656a63096f1b76955db0a
Curtis Robert <crobert@splunk.com>
2023-07-19 08:51:00 -0700

    Try base case without any temp dir

--- tests/receivers/jmxreceiver/jmxreceiver_test.go
+++ tests/receivers/jmxreceiver/jmxreceiver_test.go
@@ -12,17 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-////go:build integration
+//go:build integration
 
 package tests
 
 import (
-	"os"
 	"path"
-	"path/filepath"
 	"testing"
-
-	"github.com/stretchr/testify/require"
 
 	"github.com/signalfx/splunk-otel-collector/tests/testutils"
 )
@@ -41,16 +37,14 @@ func TestJMXReceiverProvidesAllJVMMetrics(t *testing.T) {
 			func(collector testutils.Collector) testutils.Collector {
 				// JMX requires a local directory that can be written to, so we must mount a local dir
 				// that the collector has write access to.
-				tmp_dir := "/etc/otel/collector/tmp"
-				local_tmp := filepath.Join(".", "tmp")
-				os.MkdirAll(local_tmp, 1755)
-				defer os.RemoveAll(local_tmp)
+				//tmp_dir := "/etc/otel/collector/tmp"
+				//local_tmp := filepath.Join(".", "tmp")
+				//os.MkdirAll(local_tmp, 1755)
+				//defer os.RemoveAll(local_tmp)
 
-				mount_dir, err := filepath.Abs(local_tmp)
-				require.NoError(t, err)
-				return collector.WithEnv(map[string]string{
-					"TMPDIR": tmp_dir,
-				}).WithMount(mount_dir, tmp_dir).WillFail(true)
+				//mount_dir, err := filepath.Abs(local_tmp)
+				//require.NoError(t, err)
+				return collector.WillFail(true)
 			},
 		})
 }

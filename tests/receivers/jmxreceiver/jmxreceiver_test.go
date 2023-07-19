848d616a5b10eb5ced0a3435231fe250bbcaf187
Curtis Robert <crobert@splunk.com>
2023-07-19 14:07:25 -0700

    Create tmp dir in scratch docker image

--- tests/receivers/jmxreceiver/jmxreceiver_test.go
+++ tests/receivers/jmxreceiver/jmxreceiver_test.go
@@ -17,12 +17,8 @@
 package tests
 
 import (
-	"os"
 	"path"
-	"path/filepath"
 	"testing"
-
-	"github.com/stretchr/testify/require"
 
 	"github.com/signalfx/splunk-otel-collector/tests/testutils"
 )
@@ -35,12 +31,12 @@ func TestJMXReceiverProvidesAllJVMMetrics(t *testing.T) {
 		).WithExposedPorts("7199:7199").WithName("jmx").WillWaitForPorts("7199"),
 	}
 
-	tmp_dir := "/etc/otel/collector/tmp"
-	local_tmp := filepath.Join(".", "tmp")
-	mount_dir, err := filepath.Abs(local_tmp)
-	require.NoError(t, err)
-	os.MkdirAll(mount_dir, 1777)
-	defer os.RemoveAll(local_tmp)
+	//tmp_dir := "/etc/otel/collector/tmp"
+	//local_tmp := filepath.Join(".", "tmp")
+	//mount_dir, err := filepath.Abs(local_tmp)
+	//require.NoError(t, err)
+	//os.MkdirAll(mount_dir, 1777)
+	//defer os.RemoveAll(local_tmp)
 
 	testutils.AssertAllMetricsReceived(t, "all.yaml",
 		"all_metrics_config.yaml", containers,
@@ -49,9 +45,7 @@ func TestJMXReceiverProvidesAllJVMMetrics(t *testing.T) {
 				// JMX requires a local directory that can be written to, so we must mount a local dir
 				// that the collector has write access to.
 
-				return collector.WithEnv(map[string]string{
-					"TMPDIR": tmp_dir,
-				}).WithMount(mount_dir, tmp_dir).WillFail(true)
+				return collector.WillFail(true)
 			},
 		})
 }

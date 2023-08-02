4856c43b6969f32f228725fafb2f68dab0267fe2
Ryan Fitzpatrick <rfitzpatrick@splunk.com>
2023-08-02 13:25:25 +0000

    remove unnecesary fixtures and mount

--- tests/receivers/jmxreceiver/jmxreceiver_test.go
+++ tests/receivers/jmxreceiver/jmxreceiver_test.go
@@ -17,7 +17,7 @@
 package tests
 
 import (
-	"path"
+	"path/filepath"
 	"testing"
 
 	"github.com/signalfx/splunk-otel-collector/tests/testutils"
@@ -26,33 +26,16 @@ import (
 func TestJMXReceiverProvidesAllJVMMetrics(t *testing.T) {
 	// JMX metrics are missing when running on arm so the test only checks a subset
 	// of what would be received otherwise.
-	expected_metrics_file := "all.yaml"
+	expectedMetrics := "all.yaml"
 	if testutils.CollectorImageIsForArm(t) {
-		expected_metrics_file = "arm64.yaml"
+		expectedMetrics = "arm64.yaml"
 	}
 
 	containers := []testutils.Container{
 		testutils.NewContainer().WithContext(
-			path.Join(".", "testdata", "server"),
+			filepath.Join("..", "smartagent", "collectd-cassandra", "testdata", "server"),
 		).WithExposedPorts("7199:7199").WithName("jmx").WillWaitForPorts("7199"),
 	}
 
-	testutils.AssertAllMetricsReceived(t, expected_metrics_file,
-		"all_metrics_config.yaml", containers,
-		[]testutils.CollectorBuilder{
-			func(collector testutils.Collector) testutils.Collector {
-				return collector
-				// // JMX requires a local directory that can be written to, so we must mount a local dir
-				// // that the collector has write access to.
-				// tmp_dir := "/etc/otel/collector/tmp"
-				// local_tmp := os.Getenv("TMPDIR")
-				// if local_tmp == "" {
-				// 	local_tmp = "/tmp"
-				// }
-
-				//return collector.WithEnv(map[string]string{
-				//	"TMPDIR": tmp_dir,
-				//}).WithMount(local_tmp, tmp_dir).WillFail(true)
-			},
-		})
+	testutils.AssertAllMetricsReceived(t, expectedMetrics, "all_metrics_config.yaml", containers, nil)
 }

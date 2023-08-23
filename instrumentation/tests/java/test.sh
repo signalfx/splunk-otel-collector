b0513e6a7dd7fb06751a92fdf833743413a7657a
Antoine Toulme <atoulme@splunk.com>
2023-08-22 18:23:05 -0700

    review with Jason, test for env vars overrides

--- instrumentation/tests/java/test.sh
+++ instrumentation/tests/java/test.sh
@@ -19,9 +19,11 @@ echo "============================"
 echo "Test presence of OTEL_RESOURCE_ATTRIBUTES"
 echo "$OUTPUT" | grep "OTEL_RESOURCE_ATTRIBUTES=foo=bar,this=that" > /dev/null && echo "Test passes"  || exit 1
 echo "Test presence of OTEL_SERVICE_NAME"
-echo "$OUTPUT" | grep "OTEL_SERVICE_NAME=myservice" > /dev/null && echo "Test passes"  || exit 1
+echo "$OUTPUT" | grep "OTEL_SERVICE_NAME=iknowmyownservicename" > /dev/null && echo "Test passes"  || exit 1
 echo "Test presence of SPLUNK_METRICS_ENABLED"
 echo "$OUTPUT" | grep "SPLUNK_METRICS_ENABLED=abcd" > /dev/null && echo "Test passes"  || exit 1
+echo "Test presence of ANOTHER_VAR"
+echo "$OUTPUT" | grep "ANOTHER_VAR=foo" > /dev/null && echo "Test passes"  || exit 1
 echo "Test absence of FOO"
 echo "$OUTPUT" | grep -v "FOO" > /dev/null && echo "Test passes"  || exit 1
 echo "Test absence of OTEL_EXPORTER_OTLP_ENDPOINT"

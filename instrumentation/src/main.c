0d68bebad756991bcb3ab0835bc4c06956aaac1e
Antoine Toulme <atoulme@splunk.com>
2023-08-23 18:45:31 -0700

    Make ALLOWED_ENV_VARS a single string defined through the preprocessor

--- instrumentation/src/main.c
+++ instrumentation/src/main.c
@@ -6,20 +6,14 @@
 #define MAX_LINE_LENGTH 1023
 #define MAX_LINES 50
 
+#define ALLOWED_ENV_VARS "OTEL_SERVICE_NAME", "OTEL_EXPORTER_OTLP_ENDPOINT", "OTEL_RESOURCE_ATTRIBUTES", "SPLUNK_PROFILER_ENABLED", "SPLUNK_PROFILER_MEMORY_ENABLED", "SPLUNK_METRICS_ENABLED", "JAVA_TOOL_OPTIONS", "NODE_OPTIONS"
+
+static char *const allowed_env_vars[] = {ALLOWED_ENV_VARS};
+static size_t const allowed_env_vars_size = sizeof(allowed_env_vars) / sizeof(*allowed_env_vars);
+
 // TODO change to systemd drop in file paths
 static char *const env_var_file_java = "/etc/splunk/zeroconfig_java.conf";
 static char *const env_var_file_node = "/etc/splunk/zeroconfig_node.conf";
-
-static char *const allowed_env_vars[] = {
-    "OTEL_SERVICE_NAME",
-    "OTEL_EXPORTER_OTLP_ENDPOINT",
-    "OTEL_RESOURCE_ATTRIBUTES",
-    "SPLUNK_PROFILER_ENABLED",
-    "SPLUNK_PROFILER_MEMORY_ENABLED",
-    "SPLUNK_METRICS_ENABLED",
-};
-
-const size_t allowed_env_vars_size = sizeof(allowed_env_vars) / sizeof(*allowed_env_vars);
 
 extern char *program_invocation_short_name;
 
@@ -88,8 +82,12 @@ void __attribute__((constructor)) enter() {
         char *key = buffer;
         char *value = equals + 1;
 
-        // check if key and value are valid environment key and value
+        // check if key is allowed:
+        if (strchr(key, ' ') != NULL) {
+            continue;
+        }
 
+        // check if key is allowed
         for (int i = 0; i < allowed_env_vars_size; i++) {
             if (strcmp(allowed_env_vars[i], key) == 0) {
                 setenv(key, value, 0);

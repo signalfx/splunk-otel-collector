bfd4f5430605be797fdb19be8f9d21df267171d8
Antoine Toulme <antoine@lunar-ocean.com>
2023-08-28 14:42:55 -0700

    [DNM] Zero-config C code to inject environment variables for Java and Node.js (#3514)
    * Remove sending a metric as part of auto-instrumentation (#3482)
    
    * refactor the code to inject allowed env vars
    
    * latest after pair programming
    
    * latest after review and pairing
    
    * only inject env vars for java and nodejs programs. Apply env vars from different files for java and nodejs
    
    * remove docker interactive and tty flags
    
    * wip
    
    * use arch var to set the name of the file to copy
    
    * make tests pass on arm64
    
    * try to support arm
    
    * build for the proper platform
    
    * fix package build
    
    * review with Jason, test for env vars overrides
    
    * Make ALLOWED_ENV_VARS a single string defined through the preprocessor
    
    * Update instrumentation/tests/java/Dockerfile
    
    Co-authored-by: Jeff Cheng <83052155+jeffreyc-splunk@users.noreply.github.com>
    
    * load preload file same way as java test
    
    * move file locations to macros
    
    * use n functions to handle strings when possible
    
    ---------
    
    Co-authored-by: Jeff Cheng <83052155+jeffreyc-splunk@users.noreply.github.com>

---
+++ instrumentation/tests/java/Main.java
@@ -0,0 +1,10 @@
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<String, String> env = System.getenv();
+        for (String envName : env.keySet()) {
+            System.out.format("%s=%s%n", envName, env.get(envName));
+        }
+    }
+}

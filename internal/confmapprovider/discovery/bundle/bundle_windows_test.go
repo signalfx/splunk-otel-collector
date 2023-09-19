60b62ba6a0a805adbe3eb21e6ccab75f1aeaf7f1
jeremyh <jeremyrhicks@gmail.com>
2023-09-19 09:05:58 -0400

    add to bundle_windows_test

--- internal/confmapprovider/discovery/bundle/bundle_windows_test.go
+++ internal/confmapprovider/discovery/bundle/bundle_windows_test.go
@@ -27,6 +27,7 @@ func TestBundleDir(t *testing.T) {
 	receivers, err := fs.Glob(BundledFS, "bundle.d/receivers/*.discovery.yaml")
 	require.NoError(t, err)
 	require.Equal(t, []string{
+		"bundle.d/receivers/oracledb.discovery.yaml",
 		"bundle.d/receivers/smartagent-postgresql.discovery.yaml",
 	}, receivers)
 

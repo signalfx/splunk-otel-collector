45d9299185232b0b7a760c1ce72b37315955ae10
Antoine Toulme <antoine@lunar-ocean.com>
2023-09-25 08:31:07 -0700

    remove unused utilities (#3687)

--- internal/signalfx-agent/pkg/utils/set.go
+++ internal/signalfx-agent/pkg/utils/set.go
@@ -1,20 +1,5 @@
 package utils
 
-// UniqueStrings returns a slice with the unique set of strings from the input
-func UniqueStrings(strings []string) []string {
-	unique := map[string]struct{}{}
-	for _, v := range strings {
-		unique[v] = struct{}{}
-	}
-
-	keys := make([]string, 0)
-	for k := range unique {
-		keys = append(keys, k)
-	}
-
-	return keys
-}
-
 // StringSliceToMap converts a slice of strings into a map with keys from the slice
 func StringSliceToMap(strings []string) map[string]bool {
 	// Use bool so that the user can do `if setMap[key] { ... }``
@@ -32,17 +17,6 @@ func StringSetToSlice(set map[string]bool) []string {
 	for k, ok := range set {
 		if ok {
 			out = append(out, k)
-		}
-	}
-	return out
-}
-
-// MergeStringSets merges 2+ string map sets into a single output map.
-func MergeStringSets(sets ...map[string]bool) map[string]bool {
-	out := map[string]bool{}
-	for _, ss := range sets {
-		for k, v := range ss {
-			out[k] = v
 		}
 	}
 	return out

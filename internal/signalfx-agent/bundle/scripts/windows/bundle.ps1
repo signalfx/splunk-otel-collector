06029e9ee2242e2332c2e994e00671a8d99bbcc9
jeffreyc-splunk <jeffreyc@splunk.com>
2023-10-04 11:39:09 -0400

    Update bundled python to 3.12.0

--- internal/signalfx-agent/bundle/scripts/windows/bundle.ps1
+++ internal/signalfx-agent/bundle/scripts/windows/bundle.ps1
@@ -18,8 +18,8 @@ $repoDir = "$scriptDir\..\..\..\..\.."
 . $scriptDir\common.ps1
 
 $BUILD_DIR="$repoDir\build"
-$PYTHON_VERSION="3.11.8"
-$PIP_VERSION="21.0.1"
+$PYTHON_VERSION="3.12.2"
+$PIP_VERSION="24.0.0"
 $NUGET_URL="https://aka.ms/nugetclidl"
 $NUGET_EXE="nuget.exe"
 
@@ -37,12 +37,11 @@ function get_collectd_plugins ([string]$buildDir=$BUILD_DIR) {
     $script = Resolve-Path "$scriptDir\..\get-collectd-plugins.py"
     $python = Resolve-Path "$buildDir\python\python.exe"
 
-    # workaround for https://github.com/yaml/pyyaml/issues/724
-    & $python -m pip install 'wheel==0.40.0'
-    & $python -m pip install 'Cython<3.0' 'PyYaml~=5.0' --no-build-isolation
-
     & $python -m pip install -qq -r $requirements
     if ($lastexitcode -ne 0){ throw }
+
+    # workaround for https://github.com/yaml/pyyaml/issues/724
+    & $python -m pip install 'Cython<3.0' 'PyYaml~=5.0' --no-build-isolation
 
     & $python $script $collectdPlugins
     if ($lastexitcode -ne 0){ throw }

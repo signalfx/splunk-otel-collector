8dfc01723e5920cfe797747da060a4afce0faf92
Ryan Fitzpatrick <rfitzpatrick@splunk.com>
2023-10-16 16:03:50 +0000

    move kafka test image to bitnami

--- tests/receivers/smartagent/collectd-kafka/testdata/kafka/scripts/run-broker.sh
+++ tests/receivers/smartagent/collectd-kafka/testdata/kafka/scripts/run-broker.sh
@@ -5,5 +5,5 @@ if [[ -z "$KAFKA_ZOOKEEPER_CONNECT" ]]; then
     echo "ERROR: missing mandatory config: KAFKA_ZOOKEEPER_CONNECT"
     exit 1
 fi
-"$KAFKA_BIN"/kafka-server-start.sh /opt/kafka_2.11-"$KAFKA_VERSION"/config/server.properties \
+"$KAFKA_BIN"/kafka-server-start.sh "${KAFKA_BIN}/../config/server.properties" \
     --override zookeeper.connect="$KAFKA_ZOOKEEPER_CONNECT"

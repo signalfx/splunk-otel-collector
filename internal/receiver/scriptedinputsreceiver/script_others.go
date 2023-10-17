2e022be067b2271e2aa0b7931c08b9194115cf82
weliasz <77732905+weliasz@users.noreply.github.com>
2023-10-17 22:01:48 +0200

    feat: add support for remaining scripts for scripted inputs receiver (#3792)

--- internal/receiver/scriptedinputsreceiver/script_others.go
+++ internal/receiver/scriptedinputsreceiver/script_others.go
@@ -24,14 +24,92 @@ import (
 
 const includePattern = ". \"$(dirname \"$0\")\"/common.sh"
 
+//go:embed scripts/bandwidth.sh
+var bandwidthScript string
+
 //go:embed scripts/cpu.sh
 var cpuScript string
 
 //go:embed scripts/df.sh
 var dfScript string
 
+//go:embed scripts/hardware.sh
+var hardwareScript string
+
+//go:embed scripts/interfaces.sh
+var interfacesScript string
+
+//go:embed scripts/iostat.sh
+var iostatScript string
+
+//go:embed scripts/lastlog.sh
+var lastlogScript string
+
+//go:embed scripts/lsof.sh
+var lsofScript string
+
+//go:embed scripts/netstat.sh
+var netstatScript string
+
+//go:embed scripts/nfsiostat.sh
+var nfsiostatScript string
+
+//go:embed scripts/openPorts.sh
+var openPortsScript string
+
+//go:embed scripts/openPortsEnhanced.sh
+var openPortsEnhancedScript string
+
+//go:embed scripts/package.sh
+var packageScript string
+
+//go:embed scripts/passwd.sh
+var passwdScript string
+
+//go:embed scripts/protocol.sh
+var protocolScript string
+
 //go:embed scripts/ps.sh
 var psScript string
+
+//go:embed scripts/rlog.sh
+var rlogScript string
+
+//go:embed scripts/selinuxChecker.sh
+var selinuxCheckerScript string
+
+//go:embed scripts/service.sh
+var serviceScript string
+
+//go:embed scripts/sshdChecker.sh
+var sshdCheckerScript string
+
+//go:embed scripts/time.sh
+var timeScript string
+
+//go:embed scripts/top.sh
+var topScript string
+
+//go:embed scripts/update.sh
+var updateScript string
+
+//go:embed scripts/uptime.sh
+var uptimeScript string
+
+//go:embed scripts/usersWithLoginPrivs.sh
+var usersWithLoginPrivsScript string
+
+//go:embed scripts/version.sh
+var versionScript string
+
+//go:embed scripts/vmstat.sh
+var vmstatScript string
+
+//go:embed scripts/vsftpdChecker.sh
+var vsftpdCheckerScript string
+
+//go:embed scripts/who.sh
+var whoScript string
 
 //go:embed scripts/common.sh
 var commonScript string
@@ -42,9 +120,35 @@ var scripts = func() map[string]string {
 		name    string
 		content string
 	}{
+		{"bandwidth", bandwidthScript},
 		{"cpu", cpuScript},
 		{"df", dfScript},
+		{"hardware", hardwareScript},
+		{"interfaces", interfacesScript},
+		{"iostat", iostatScript},
+		{"lastlog", lastlogScript},
+		{"lsof", lsofScript},
+		{"netstat", netstatScript},
+		{"nfsiostat", nfsiostatScript},
+		{"openPorts", openPortsScript},
+		{"openPortsEnhanced", openPortsEnhancedScript},
+		{"package", packageScript},
+		{"passwd", passwdScript},
+		{"protocol", protocolScript},
 		{"ps", psScript},
+		{"rlog", rlogScript},
+		{"selinuxChecker", selinuxCheckerScript},
+		{"service", serviceScript},
+		{"sshdChecker", sshdCheckerScript},
+		{"time", timeScript},
+		{"top", topScript},
+		{"update", updateScript},
+		{"uptime", uptimeScript},
+		{"usersWithLoginPrivs", usersWithLoginPrivsScript},
+		{"version", versionScript},
+		{"vmstat", vmstatScript},
+		{"vsftpdChecker", vsftpdCheckerScript},
+		{"who", whoScript},
 	} {
 		if _, ok := scriptsMap[s.name]; ok {
 			panic(fmt.Errorf("duplicate script_name %q detected", s.name))
--- tests/receivers/scriptedinputs/script_test.go
+++ tests/receivers/scriptedinputs/script_test.go
@@ -28,7 +28,7 @@ import (
 	"github.com/signalfx/splunk-otel-collector/tests/testutils"
 )
 
-func TestScriptReceiver(t *testing.T) {
+func TestScriptReceiverCpu(t *testing.T) {
 	tc := testutils.NewTestcase(t)
 	defer tc.PrintLogsOnFailure()
 	defer tc.ShutdownOTLPReceiverSink()
@@ -37,6 +37,149 @@ func TestScriptReceiver(t *testing.T) {
 	defer shutdown()
 
 	expectedLogs, err := telemetry.LoadResourceLogs(filepath.Join("testdata", "resource_logs", "cpu.yaml"))
+	require.NoError(t, err)
+	require.NoError(t, tc.OTLPReceiverSink.AssertAllLogsReceived(t, *expectedLogs, 10*time.Second))
+}
+
+func TestScriptReceiverDf(t *testing.T) {
+	tc := testutils.NewTestcase(t)
+	defer tc.PrintLogsOnFailure()
+	defer tc.ShutdownOTLPReceiverSink()
+
+	_, shutdown := tc.SplunkOtelCollectorProcess("script_config_df.yaml")
+	defer shutdown()
+
+	expectedLogs, err := telemetry.LoadResourceLogs(filepath.Join("testdata", "resource_logs", "df.yaml"))
+	require.NoError(t, err)
+	require.NoError(t, tc.OTLPReceiverSink.AssertAllLogsReceived(t, *expectedLogs, 10*time.Second))
+}
+
+func TestScriptReceiverHardware(t *testing.T) {
+	tc := testutils.NewTestcase(t)
+	defer tc.PrintLogsOnFailure()
+	defer tc.ShutdownOTLPReceiverSink()
+
+	_, shutdown := tc.SplunkOtelCollectorProcess("script_config_hardware.yaml")
+	defer shutdown()
+
+	expectedLogs, err := telemetry.LoadResourceLogs(filepath.Join("testdata", "resource_logs", "hardware.yaml"))
+	require.NoError(t, err)
+	require.NoError(t, tc.OTLPReceiverSink.AssertAllLogsReceived(t, *expectedLogs, 10*time.Second))
+}
+
+func TestScriptReceiverInterfaces(t *testing.T) {
+	tc := testutils.NewTestcase(t)
+	defer tc.PrintLogsOnFailure()
+	defer tc.ShutdownOTLPReceiverSink()
+
+	_, shutdown := tc.SplunkOtelCollectorProcess("script_config_interfaces.yaml")
+	defer shutdown()
+
+	expectedLogs, err := telemetry.LoadResourceLogs(filepath.Join("testdata", "resource_logs", "interfaces.yaml"))
+	require.NoError(t, err)
+	require.NoError(t, tc.OTLPReceiverSink.AssertAllLogsReceived(t, *expectedLogs, 10*time.Second))
+}
+
+func TestScriptReceiverIostat(t *testing.T) {
+	tc := testutils.NewTestcase(t)
+	defer tc.PrintLogsOnFailure()
+	defer tc.ShutdownOTLPReceiverSink()
+
+	_, shutdown := tc.SplunkOtelCollectorProcess("script_config_iostat.yaml")
+	defer shutdown()
+
+	expectedLogs, err := telemetry.LoadResourceLogs(filepath.Join("testdata", "resource_logs", "iostat.yaml"))
+	require.NoError(t, err)
+	require.NoError(t, tc.OTLPReceiverSink.AssertAllLogsReceived(t, *expectedLogs, 10*time.Second))
+}
+
+func TestScriptReceiverLsof(t *testing.T) {
+	tc := testutils.NewTestcase(t)
+	defer tc.PrintLogsOnFailure()
+	defer tc.ShutdownOTLPReceiverSink()
+
+	_, shutdown := tc.SplunkOtelCollectorProcess("script_config_lsof.yaml")
+	defer shutdown()
+
+	expectedLogs, err := telemetry.LoadResourceLogs(filepath.Join("testdata", "resource_logs", "lsof.yaml"))
+	require.NoError(t, err)
+	require.NoError(t, tc.OTLPReceiverSink.AssertAllLogsReceived(t, *expectedLogs, 10*time.Second))
+}
+
+func TestScriptReceiverNetstat(t *testing.T) {
+	tc := testutils.NewTestcase(t)
+	defer tc.PrintLogsOnFailure()
+	defer tc.ShutdownOTLPReceiverSink()
+
+	_, shutdown := tc.SplunkOtelCollectorProcess("script_config_netstat.yaml")
+	defer shutdown()
+
+	expectedLogs, err := telemetry.LoadResourceLogs(filepath.Join("testdata", "resource_logs", "netstat.yaml"))
+	require.NoError(t, err)
+	require.NoError(t, tc.OTLPReceiverSink.AssertAllLogsReceived(t, *expectedLogs, 10*time.Second))
+}
+
+func TestScriptReceiverOpenPorts(t *testing.T) {
+	tc := testutils.NewTestcase(t)
+	defer tc.PrintLogsOnFailure()
+	defer tc.ShutdownOTLPReceiverSink()
+
+	_, shutdown := tc.SplunkOtelCollectorProcess("script_config_openPorts.yaml")
+	defer shutdown()
+
+	expectedLogs, err := telemetry.LoadResourceLogs(filepath.Join("testdata", "resource_logs", "openPorts.yaml"))
+	require.NoError(t, err)
+	require.NoError(t, tc.OTLPReceiverSink.AssertAllLogsReceived(t, *expectedLogs, 10*time.Second))
+}
+
+func TestScriptReceiverPackage(t *testing.T) {
+	tc := testutils.NewTestcase(t)
+	defer tc.PrintLogsOnFailure()
+	defer tc.ShutdownOTLPReceiverSink()
+
+	_, shutdown := tc.SplunkOtelCollectorProcess("script_config_package.yaml")
+	defer shutdown()
+
+	expectedLogs, err := telemetry.LoadResourceLogs(filepath.Join("testdata", "resource_logs", "package.yaml"))
+	require.NoError(t, err)
+	require.NoError(t, tc.OTLPReceiverSink.AssertAllLogsReceived(t, *expectedLogs, 10*time.Second))
+}
+
+func TestScriptReceiverProtocol(t *testing.T) {
+	tc := testutils.NewTestcase(t)
+	defer tc.PrintLogsOnFailure()
+	defer tc.ShutdownOTLPReceiverSink()
+
+	_, shutdown := tc.SplunkOtelCollectorProcess("script_config_protocol.yaml")
+	defer shutdown()
+
+	expectedLogs, err := telemetry.LoadResourceLogs(filepath.Join("testdata", "resource_logs", "protocol.yaml"))
+	require.NoError(t, err)
+	require.NoError(t, tc.OTLPReceiverSink.AssertAllLogsReceived(t, *expectedLogs, 10*time.Second))
+}
+
+func TestScriptReceiverPs(t *testing.T) {
+	tc := testutils.NewTestcase(t)
+	defer tc.PrintLogsOnFailure()
+	defer tc.ShutdownOTLPReceiverSink()
+
+	_, shutdown := tc.SplunkOtelCollectorProcess("script_config_ps.yaml")
+	defer shutdown()
+
+	expectedLogs, err := telemetry.LoadResourceLogs(filepath.Join("testdata", "resource_logs", "ps.yaml"))
+	require.NoError(t, err)
+	require.NoError(t, tc.OTLPReceiverSink.AssertAllLogsReceived(t, *expectedLogs, 10*time.Second))
+}
+
+func TestScriptReceiverTop(t *testing.T) {
+	tc := testutils.NewTestcase(t)
+	defer tc.PrintLogsOnFailure()
+	defer tc.ShutdownOTLPReceiverSink()
+
+	_, shutdown := tc.SplunkOtelCollectorProcess("script_config_top.yaml")
+	defer shutdown()
+
+	expectedLogs, err := telemetry.LoadResourceLogs(filepath.Join("testdata", "resource_logs", "top.yaml"))
 	require.NoError(t, err)
 	require.NoError(t, tc.OTLPReceiverSink.AssertAllLogsReceived(t, *expectedLogs, 10*time.Second))
 }

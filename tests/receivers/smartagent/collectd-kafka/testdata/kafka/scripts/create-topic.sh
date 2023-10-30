758a2bfaf485b0096fac3cb207dabc87b14e5cb1
Ryan Fitzpatrick <10867373+rmfitzpatrick@users.noreply.github.com>
2023-10-30 10:11:13 -0400

    update kafka testing to 3.6.0 (#3840)

--- tests/receivers/smartagent/collectd-kafka/testdata/kafka/scripts/create-topic.sh
+++ tests/receivers/smartagent/collectd-kafka/testdata/kafka/scripts/create-topic.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-if [[ -z "$KAFKA_ZOOKEEPER_CONNECT" ]]; then
-    echo "ERROR: missing mandatory config: KAFKA_ZOOKEEPER_CONNECT"
+if [[ -z "$KAFKA_BROKER" ]]; then
+    echo "ERROR: missing mandatory config: KAFKA_BROKER"
     exit 1
 fi
 "$KAFKA_BIN"/kafka-topics.sh \
     --create \
-    --zookeeper "$KAFKA_ZOOKEEPER_CONNECT" \
+    --bootstrap-server "$KAFKA_BROKER" \
     --replication-factor 1 \
     --partitions 1 \
     --topic sfx-employee \

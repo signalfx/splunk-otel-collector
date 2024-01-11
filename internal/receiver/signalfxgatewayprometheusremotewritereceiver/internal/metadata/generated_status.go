e87649044cf21f2cb898e4a977591c5a2993c4c9
Ryan Fitzpatrick <rfitzpatrick@splunk.com>
2024-01-11 19:31:54 +0000

    make generate-metrics

--- internal/receiver/signalfxgatewayprometheusremotewritereceiver/internal/metadata/generated_status.go
+++ internal/receiver/signalfxgatewayprometheusremotewritereceiver/internal/metadata/generated_status.go
@@ -4,9 +4,19 @@ package metadata
 
 import (
 	"go.opentelemetry.io/collector/component"
+	"go.opentelemetry.io/otel/metric"
+	"go.opentelemetry.io/otel/trace"
 )
 
 const (
 	Type             = "signalfxgatewayprometheusremotewrite"
 	MetricsStability = component.StabilityLevelDevelopment
 )
+
+func Meter(settings component.TelemetrySettings) metric.Meter {
+	return settings.MeterProvider.Meter("otelcol/signalfxgatewayprometheusremotewritereceiver")
+}
+
+func Tracer(settings component.TelemetrySettings) trace.Tracer {
+	return settings.TracerProvider.Tracer("otelcol/signalfxgatewayprometheusremotewritereceiver")
+}

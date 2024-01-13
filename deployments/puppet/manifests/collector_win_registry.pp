2c3af0900e392e7ec58b3e4ffdb2957619c71798
Paulo Janotti <pjanotti@splunk.com>
2024-01-12 20:15:41 -0800

    Do not re-use registry_key

--- deployments/puppet/manifests/collector_win_registry.pp
+++ deployments/puppet/manifests/collector_win_registry.pp
@@ -28,10 +28,10 @@ class splunk_otel_collector::collector_win_registry () {
 
   $collector_env_vars.sort()
 
-  registry_value { "${splunk_otel_collector::registry_key}\\Environment":
+  registry_value { "HKLM\\SYSTEM\\CurrentControlSet\\Services\\splunk-otel-collector\\Environment":
     ensure  => 'present',
     type    => multi_string,
     data    => $collector_env_vars,
-    require => Registry_key[$splunk_otel_collector::registry_key],
+    require => Registry_key["HKLM\\SYSTEM\\CurrentControlSet\\Services\\splunk-otel-collector"],
   }
 }

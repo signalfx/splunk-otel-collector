4f18580e0f6635a68ac7424c1164888c867946a1
Paulo Janotti <pjanotti@splunk.com>
2024-01-12 18:19:04 -0800

    Try extension fixes

--- deployments/puppet/manifests/collector_win_registry.pp
+++ deployments/puppet/manifests/collector_win_registry.pp
@@ -1,6 +1,6 @@
 # Class for setting the registry values for the splunk-otel-collector service
 class splunk_otel_collector::collector_win_registry () {
-  collector_env_vars = [
+  $collector_env_vars = [
     "SPLUNK_ACCESS_TOKEN=${splunk_otel_collector::splunk_access_token}",
     "SPLUNK_API_URL=${splunk_otel_collector::splunk_api_url}",
     "SPLUNK_BUNDLE_DIR=${splunk_otel_collector::splunk_bundle_dir}",
@@ -14,18 +14,19 @@ class splunk_otel_collector::collector_win_registry () {
     "SPLUNK_TRACE_URL=${splunk_otel_collector::splunk_trace_url}",
   ]
 
-  unless $splunk_otel_collector::splunk_ballast_size_mib.to_s.strip().empty? {
-    collector_env_vars.push("SPLUNK_BALLAST_SIZE_MIB=${splunk_otel_collector::splunk_ballast_size_mib}")
+  unless $splunk_otel_collector::splunk_ballast_size_mib.to_s.strip() {
+    $collector_env_vars.push("SPLUNK_BALLAST_SIZE_MIB=${splunk_otel_collector::splunk_ballast_size_mib}")
   }
-  unless $splunk_otel_collector::splunk_listen_interface.to_s.strip().empty? {
-    collector_env_vars.push("SPLUNK_LISTEN_INTERFACE=${splunk_otel_collector::splunk_listen_interface}")
+
+  unless $splunk_otel_collector::splunk_listen_interface.to_s.strip() {
+    $collector_env_vars.push("SPLUNK_LISTEN_INTERFACE=${splunk_otel_collector::splunk_listen_interface}")
   }
 
   $splunk_otel_collector::collector_additional_env_vars.each |$var, $value| {
-    collector_env_vars |= "${var}=${value}"
+    $collector_env_vars.push("${var}=${value}")
   }
 
-  collector_env_vars.sort!
+  $collector_env_vars.sort()
 
   $registry_key = 'HKLM\SYSTEM\CurrentControlSet\Services\splunk-otel-collector'
 

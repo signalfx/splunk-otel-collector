9d19cdadc5326334934d5d56ab12a50d51cfa6d4
james hughes <jameshughes@splunk.com>
2024-02-20 15:09:21 -0800

    regenerate metrics

--- internal/receiver/databricksreceiver/internal/metadata/generated_status.go
+++ internal/receiver/databricksreceiver/internal/metadata/generated_status.go
@@ -8,8 +8,11 @@ import (
 	"go.opentelemetry.io/otel/trace"
 )
 
+var (
+	Type = component.MustNewType("databricks")
+)
+
 const (
-	Type             = "databricks"
 	MetricsStability = component.StabilityLevelDevelopment
 )
 

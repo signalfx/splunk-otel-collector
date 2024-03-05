b4a06fe2db8e1f29b71a25c2a7aa41ca970350d3
Paulo Janotti <pjanotti@splunk.com>
2024-03-05 02:19:26 -0800

    databricks receiver generated data without fieldalignment -fix

--- internal/receiver/databricksreceiver/internal/metadata/generated_metrics.go
+++ internal/receiver/databricksreceiver/internal/metadata/generated_metrics.go
@@ -7673,8 +7673,11 @@ func newMetricDatabricksTasksScheduleStatus(cfg MetricConfig) metricDatabricksTa
 // MetricsBuilder provides an interface for scrapers to report metrics while taking care of all the transformations
 // required to produce metric representation defined in metadata and user config.
 type MetricsBuilder struct {
-	metricsBuffer                                                                                                            pmetric.Metrics
-	buildInfo                                                                                                                component.BuildInfo
+	config                                                                                                                   MetricsBuilderConfig // config of the metrics builder.
+	startTime                                                                                                                pcommon.Timestamp    // start time that will be applied to all recorded data points.
+	metricsCapacity                                                                                                          int                  // maximum observed number of metrics per resource.
+	metricsBuffer                                                                                                            pmetric.Metrics      // accumulates metrics data before emitting.
+	buildInfo                                                                                                                component.BuildInfo  // contains version information.
 	metricDatabricksJobsActiveTotal                                                                                          metricDatabricksJobsActiveTotal
 	metricDatabricksJobsRunDuration                                                                                          metricDatabricksJobsRunDuration
 	metricDatabricksJobsScheduleStatus                                                                                       metricDatabricksJobsScheduleStatus
@@ -7815,9 +7818,6 @@ type MetricsBuilder struct {
 	metricDatabricksSparkTimerLiveListenerBusQueueStreamsListenerProcessingTime                                              metricDatabricksSparkTimerLiveListenerBusQueueStreamsListenerProcessingTime
 	metricDatabricksTasksRunDuration                                                                                         metricDatabricksTasksRunDuration
 	metricDatabricksTasksScheduleStatus                                                                                      metricDatabricksTasksScheduleStatus
-	startTime                                                                                                                pcommon.Timestamp
-	metricsCapacity                                                                                                          int
-	config                                                                                                                   MetricsBuilderConfig
 }
 
 // metricBuilderOption applies changes to default metrics builder.

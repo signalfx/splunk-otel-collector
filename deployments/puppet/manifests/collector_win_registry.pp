3c3b745f1d865f040faf272a6f02804207c45ca0
Samiur Arif <samiura@splunk.com>
2024-04-03 08:36:00 -0700

    Co-authored-by: Jeff Cheng <83052155+jeffreyc-splunk@users.noreply.github.com>
    addressing previous PR comments.

--- deployments/puppet/manifests/collector_win_registry.pp
+++ deployments/puppet/manifests/collector_win_registry.pp
@@ -16,16 +16,16 @@ class splunk_otel_collector::collector_win_registry () {
     "SPLUNK_REALM=${splunk_otel_collector::splunk_realm}",
     "SPLUNK_TRACE_URL=${splunk_otel_collector::splunk_trace_url}",
   ]
-  + if ($splunk_otel_collector::params::collector_version != 'latest' and
-  versioncmp($splunk_otel_collector::params::collector_version, '0.97.0') < 0 ) and
+  + if ($splunk_otel_collector::collector_version != 'latest' and
+  versioncmp($splunk_otel_collector::collector_version, '0.97.0') < 0 ) and
   !$splunk_otel_collector::splunk_ballast_size_mib.strip().empty()
   {
     ["SPLUNK_BALLAST_SIZE_MIB=${splunk_otel_collector::splunk_ballast_size_mib}"]
   } else {
     []
   }
-  + if ($splunk_otel_collector::params::collector_version == 'latest' and
-  versioncmp($splunk_otel_collector::params::collector_version, '0.97.0') >= 0 ) and
+  + if ($splunk_otel_collector::collector_version == 'latest' or
+  versioncmp($splunk_otel_collector::collector_version, '0.97.0') >= 0 ) and
   !$splunk_otel_collector::gomemlimit.strip().empty()
   {
     ["GOMEMLIMIT=${splunk_otel_collector::gomemlimit}"]

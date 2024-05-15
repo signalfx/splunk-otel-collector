29cc01f1345990aa3112531eab0e51dea2b7fbb6
Dmitry Anoshin <anoshindx@gmail.com>
2024-05-15 01:11:16 -0700

    [receiver/discovery] Remove `append_pattern` option from log statements
    Remove `append_pattern` option from log evaluation statements.
      - The matched log message is now set as `discovery.matched_log` entity attributes instead of being appended to the `discovery.message` attribute.
      - The matched log fields like `caller` and `stacktrace` are not sent as attributes anymore since it's not relevant to the emitted entities anymore and don't provide any value to the end users

--- internal/receiver/discoveryreceiver/statement_evaluator_test.go
+++ internal/receiver/discoveryreceiver/statement_evaluator_test.go
@@ -15,9 +15,7 @@
 package discoveryreceiver
 
 import (
-	"fmt"
-	"runtime"
-	"strings"
+	"errors"
 	"sync"
 	"testing"
 	"time"
@@ -40,109 +38,83 @@ func TestStatementEvaluation(t *testing.T) {
 		{name: "expr", match: Match{Expr: "message == 'desired.statement' && ExprEnv['field.one'] == 'field.one.value' && field_two contains 'two.value'"}},
 	} {
 		t.Run(tc.name, func(t *testing.T) {
-			for _, appendPattern := range []bool{true, false} {
-				t.Run(fmt.Sprintf("append_%v", appendPattern), func(t *testing.T) {
-					match := tc.match
-					match.Record = &LogRecord{
-						Body:          "desired body content",
-						AppendPattern: appendPattern,
-						Attributes: map[string]string{
-							"attr.one": "attr.one.value", "attr.two": "attr.two.value",
+			match := tc.match
+			match.Record = &LogRecord{
+				Body: "desired body content",
+				Attributes: map[string]string{
+					"attr.one": "attr.one.value", "attr.two": "attr.two.value",
+				},
+			}
+			for _, status := range discovery.StatusTypes {
+				match.Status = status
+				t.Run(string(status), func(t *testing.T) {
+					observerID := component.MustNewIDWithName("an_observer", "observer.name")
+					cfg := &Config{
+						Receivers: map[component.ID]ReceiverEntry{
+							component.MustNewIDWithName("a_receiver", "receiver.name"): {
+								Rule:   mustNewRule(`type == "container"`),
+								Status: &Status{Statements: []Match{match}},
+							},
 						},
+						WatchObservers: []component.ID{observerID},
 					}
-					for _, status := range discovery.StatusTypes {
-						match.Status = status
-						t.Run(string(status), func(t *testing.T) {
-							observerID := component.MustNewIDWithName("an_observer", "observer.name")
-							cfg := &Config{
-								Receivers: map[component.ID]ReceiverEntry{
-									component.MustNewIDWithName("a_receiver", "receiver.name"): {
-										Rule:   mustNewRule(`type == "container"`),
-										Status: &Status{Statements: []Match{match}},
-									},
-								},
-								WatchObservers: []component.ID{observerID},
-							}
-							require.NoError(t, cfg.Validate())
+					require.NoError(t, cfg.Validate())
 
-							// If debugging tests, replace the Nop Logger with a test instance to see
-							// all statements. Not in regular use to avoid spamming output.
-							// logger := zaptest.NewLogger(t)
-							logger := zap.NewNop()
-							cStore := newCorrelationStore(logger, time.Hour)
+					// If debugging tests, replace the Nop Logger with a test instance to see
+					// all statements. Not in regular use to avoid spamming output.
+					// logger := zaptest.NewLogger(t)
+					logger := zap.NewNop()
+					cStore := newCorrelationStore(logger, time.Hour)
 
-							emitCh := cStore.EmitCh()
-							emitWG := sync.WaitGroup{}
-							emitWG.Add(1)
-							go func() {
-								<-emitCh
-								emitWG.Done()
-							}()
+					emitCh := cStore.EmitCh()
+					emitWG := sync.WaitGroup{}
+					emitWG.Add(1)
+					go func() {
+						<-emitCh
+						emitWG.Done()
+					}()
 
-							receiverID := component.MustNewIDWithName("a_receiver", "receiver.name")
-							endpointID := observer.EndpointID("endpoint.id")
-							cStore.UpdateEndpoint(observer.Endpoint{ID: endpointID}, receiverID, observerID)
+					receiverID := component.MustNewIDWithName("a_receiver", "receiver.name")
+					endpointID := observer.EndpointID("endpoint.id")
+					cStore.UpdateEndpoint(observer.Endpoint{ID: endpointID}, receiverID, observerID)
 
-							se, err := newStatementEvaluator(logger, component.MustNewID("some_type"), cfg, cStore)
-							require.NoError(t, err)
+					se, err := newStatementEvaluator(logger, component.MustNewID("some_type"), cfg, cStore)
+					require.NoError(t, err)
 
-							evaluatedLogger := se.evaluatedLogger.With(
-								zap.String("name", `a_receiver/receiver.name/receiver_creator/rc.name/{endpoint=""}/endpoint.id`),
-							)
+					evaluatedLogger := se.evaluatedLogger.With(
+						zap.String("name", `a_receiver/receiver.name/receiver_creator/rc.name/{endpoint=""}/endpoint.id`),
+					)
 
-							for _, statement := range []string{
-								"undesired.statement",
-								"another.undesired.statement",
-								"desired.statement",
-								"desired.statement",
-								"desired.statement",
-							} {
-								evaluatedLogger.Info(
-									statement,
-									zap.String("field.one", "field.one.value"),
-									zap.String("field_two", "field.two.value"),
-								)
-							}
-
-							// wait for the emit channel to be processed
-							emitWG.Wait()
-
-							attrs := cStore.Attrs(endpointID)
-
-							// Validate "caller" attribute
-							callerAttr, ok := attrs["caller"]
-							require.True(t, ok)
-							_, expectedFile, _, _ := runtime.Caller(0)
-							// runtime doesn't use os.PathSeparator
-							splitPath := strings.Split(expectedFile, "/")
-							expectedCaller := splitPath[len(splitPath)-1]
-							require.Contains(t, callerAttr, expectedCaller)
-							delete(attrs, "caller")
-
-							// Validate the rest of the attributes
-							expectedMsg := "desired body content"
-							if match.Record.AppendPattern {
-								if match.Strict != "" {
-									expectedMsg = fmt.Sprintf("%s (evaluated \"desired.statement\")", expectedMsg)
-								} else {
-									expectedMsg = fmt.Sprintf("%s (evaluated \"{\\\"field.one\\\":\\\"field.one.value\\\",\\\"field_two\\\":\\\"field.two.value\\\",\\\"message\\\":\\\"desired.statement\\\"}\")", expectedMsg)
-								}
-							}
-							require.Equal(t, map[string]string{
-								"discovery.observer.id":   "an_observer/observer.name",
-								"discovery.receiver.name": "receiver.name",
-								"discovery.receiver.rule": `type == "container"`,
-								"discovery.receiver.type": "a_receiver",
-								"discovery.status":        string(status),
-								"discovery.message":       expectedMsg,
-								"name":                    `a_receiver/receiver.name/receiver_creator/rc.name/{endpoint=""}/endpoint.id`,
-								"attr.one":                "attr.one.value",
-								"attr.two":                "attr.two.value",
-								"field.one":               "field.one.value",
-								"field_two":               "field.two.value",
-							}, attrs)
-						})
+					for _, statement := range []string{
+						"undesired.statement",
+						"another.undesired.statement",
+						"desired.statement",
+						"desired.statement",
+						"desired.statement",
+					} {
+						evaluatedLogger.Info(
+							statement,
+							zap.String("field.one", "field.one.value"),
+							zap.String("field_two", "field.two.value"),
+							zap.Error(errors.New("some error")),
+						)
 					}
+
+					// wait for the emit channel to be processed
+					emitWG.Wait()
+
+					// Validate the attributes
+					require.Equal(t, map[string]string{
+						"discovery.observer.id":   "an_observer/observer.name",
+						"discovery.receiver.name": "receiver.name",
+						"discovery.receiver.rule": `type == "container"`,
+						"discovery.receiver.type": "a_receiver",
+						"discovery.status":        string(status),
+						"discovery.message":       "desired body content",
+						"discovery.matched_log":   "desired.statement (error: some error)",
+						"attr.one":                "attr.one.value",
+						"attr.two":                "attr.two.value",
+					}, cStore.Attrs(endpointID))
 				})
 			}
 		})

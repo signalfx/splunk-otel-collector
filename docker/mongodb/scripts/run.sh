976488c4434e2981de18b585c1767f4d0dca05c0
Samiur Arif <samiura@splunk.com>
2024-06-20 08:29:17 -0700

    Added Mongodb versions 6.0 and 7.0 to the list of auto discovery integration tests

--- docker/mongodb/scripts/run.sh
+++ docker/mongodb/scripts/run.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
 
+
+run_mongo_commandline () {
+if (( $(echo "$MONGO_MAJOR > 5.9" | bc -l) )); then
+    nohup gosu mongodb mongosh admin --eval "help" > /dev/null 2>&1
+else
+    nohup gosu mongodb mongo admin --eval "help" > /dev/null 2>&1
+fi
+}
+
 sleep 5
 
 chown -R mongodb:mongodb /home/mongodb
 
 nohup gosu mongodb mongod --dbpath=/data/db &
 
-nohup gosu mongodb mongo admin --eval "help" > /dev/null 2>&1
+run_mongo_commandline
+
 RET=$?
 
 while [[ "$RET" -ne 0 ]]; do
   echo "Waiting for MongoDB to start..."
-  mongo admin --eval "help" > /dev/null 2>&1
+  run_mongo_commandline
   RET=$?
   sleep 2
 done
 
-bash /home/mongodb/scripts/setup_user.sh
-
+if (( $(echo "${MONGO_MAJOR} > 5.9" | bc -l) )); then
+    bash /home/mongodb/scripts/setup_user_newer_ver.sh
+else
+    bash /home/mongodb/scripts/setup_user.sh
+fi
 
 gosu mongodb mongod --dbpath=/data/db --config mongod.conf

9e7e541bc86c6ac70b440d343e0c3a57754d9213
Dmitry Anoshin <anoshindx@gmail.com>
2024-08-05 15:31:56 -0700

    See what fails without -confmap.strictlyTypedInput

--- internal/settings/settings.go
+++ internal/settings/settings.go
@@ -80,7 +80,6 @@ var DefaultAgentConfigWindows = func() string {
 
 var defaultFeatureGates = []string{
 	// Remove the feature gates once we deprecate our env var expansion mechanism and adopt the upstream one.
-	"-confmap.strictlyTypedInput",
 	"-confmap.unifyEnvVarExpansion",
 }
 

45bb44bab7183d15a300488f17f791d3bbed08ee
Paulo Janotti <pjanotti@splunk.com>
2024-09-05 09:09:45 -0700

    Disable OOTB Apache discovery
    Disabling it since it requires configuration and doesn't work OOTB.

--- internal/confmapprovider/discovery/bundle/bundledfs_windows_test.go
+++ internal/confmapprovider/discovery/bundle/bundledfs_windows_test.go
@@ -27,7 +27,6 @@ func TestBundleDir(t *testing.T) {
 	receivers, err := fs.Glob(BundledFS, "bundle.d/receivers/*.discovery.yaml")
 	require.NoError(t, err)
 	require.Equal(t, []string{
-		"bundle.d/receivers/apachewebserver.discovery.yaml",
 		"bundle.d/receivers/jmx-cassandra.discovery.yaml",
 		"bundle.d/receivers/kafkametrics.discovery.yaml",
 		"bundle.d/receivers/mongodb.discovery.yaml",

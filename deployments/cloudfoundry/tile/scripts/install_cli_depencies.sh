86c27393d83c0174fd8d638024d65d22abd264a1
Curtis Robert <crobert@splunk.com>
2024-10-07 09:57:22 -0700

    [chore][Tanzu Tile] Add URLs for install_cli_depencies.sh (#5450)
    * Update install_cli_depencies.sh
    
    * Update deployments/cloudfoundry/tile/scripts/install_cli_depencies.sh
    
    * Update deployments/cloudfoundry/tile/scripts/install_cli_depencies.sh
    
    * Update deployments/cloudfoundry/tile/scripts/install_cli_depencies.sh

--- deployments/cloudfoundry/tile/scripts/install_cli_depencies.sh
+++ deployments/cloudfoundry/tile/scripts/install_cli_depencies.sh
@@ -1,11 +1,16 @@
-# Note: This script installs all CLI tools that are required for local Tanzu Tile related deevelopment
+# Note: This script installs all CLI tools that are required for local Tanzu Tile related development
 # This script was originally written for MacOS and has not been tested on other platforms.
 
+# https://jqlang.github.io/jq/
 brew install jq
+
+# https://www.gnu.org/software/wget/
 brew install wget
+
+# https://cloud-foundry-installation.readthedocs.io/en/latest/BOSH.html
 brew install cloudfoundry/tap/bosh-cli
 
-# Navigate to: https://github.com/cf-platform-eng/tile-generator
+# Pre-req: Navigate to https://github.com/cf-platform-eng/tile-generator
 # Download the tile and pcf release asssets, these will need to be an old version on MacOS, darwin support was dropped
 chmod +x ~/Downloads/tile_darwin-64bit
 chmod +x ~/Downloads/pcf_darwin-64bit
@@ -14,7 +19,10 @@ mv ~/Downloads/tile_darwin-64bit /usr/local/bin/tile
 
 brew tap pivotal/hammer https://github.com/pivotal/hammer
 brew install hammer
+
+# https://github.com/cloudfoundry/homebrew-tap
 brew install cloudfoundry/tap/cf-cli@8
+
 brew tap pivotal-cf/om https://github.com/pivotal-cf/om
 brew install om
 
@@ -24,6 +32,9 @@ source ~/.bash_profile
 rbenv install 3.1.3
 rbenv global 3.1.3
 ruby -v
+
+# https://github.com/cloudfoundry/cf-uaac
 gem install cf-uaac
 
+# https://github.com/pivotal/LicenseFinder
 gem install license_finder

9c22771cf06511a14234c8d2a0fc1aa010af2519
Antoine Toulme <atoulme@splunk.com>
2024-10-11 13:19:05 -0700

    Move packaging to its own top level folder

---
+++ packaging/release/gh-release-notes.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -euo pipefail
+
+get_digest() {
+    local digest_file="$1"
+    local digest=""
+
+    if [[ ! -f "$digest_file" ]]; then
+        echo "$digest_file not found" >&2
+        return 1
+    fi
+
+    digest="$( cat "$digest_file" | tr -dc '[[:print:]]' | sed 's|\[.*@\(sha256:.*\)\]|\1|' )"
+
+    if [[ ! "$digest" =~ ^sha256:[A-Fa-f0-9]{64}$ ]]; then
+        echo "Failed to get digest from $digest_file" >&2
+        return 1
+    fi
+
+    echo "$digest"
+}
+
+SCRIPT_DIR="$( cd "$( dirname ${BASH_SOURCE[0]} )" && pwd )"
+REPO_DIR="$( cd "$SCRIPT_DIR"/../../ && pwd )"
+
+VERSION="$1"
+MULTIARCH_DIGEST="$( get_digest "${REPO_DIR}/dist/multiarch_digest.txt" )"
+WINDOWS_MULTIARCH_DIGEST="$( get_digest "${REPO_DIR}/dist/windows_multiarch_digest.txt" )"
+CHANGELOG="${REPO_DIR}/CHANGELOG.md"
+
+changes="$( awk -v version="$VERSION" '/^## / { if (p) { exit }; if ($2 == version) { p=1; next } } p && NF' "$CHANGELOG" )"
+
+if [[ -z "$changes" ]] || [[ "$changes" =~ ^[[:space:]]+$ ]]; then
+  changes="Release notes in progress."
+fi
+
+cat <<EOH
+$changes
+
+> Docker Image Manifests:
+> - Linux (amd64, arm64, ppc64le) and Windows (2019 amd64, 2022 amd64):
+>   - \`quay.io/signalfx/splunk-otel-collector:${VERSION#v}\`
+>   - digest: \`$MULTIARCH_DIGEST\`
+> - Windows (2019 amd64, 2022 amd64):
+>   - \`quay.io/signalfx/splunk-otel-collector-windows:${VERSION#v}\`
+>   - digest: \`$WINDOWS_MULTIARCH_DIGEST\`
+EOH
---
+++ packaging/release/helpers/constants.py
@@ -0,0 +1,51 @@
+# Copyright 2020 Splunk, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from pathlib import Path
+
+# Artifactory
+ARTIFACTORY_URL = "https://splunk.jfrog.io/artifactory"
+ARTIFACTORY_API_URL = f"{ARTIFACTORY_URL}/api"
+ARTIFACTORY_DEB_REPO = "otel-collector-deb"
+ARTIFACTORY_DEB_REPO_URL = f"{ARTIFACTORY_URL}/{ARTIFACTORY_DEB_REPO}"
+ARTIFACTORY_RPM_REPO = "otel-collector-rpm"
+ARTIFACTORY_RPM_REPO_URL = f"{ARTIFACTORY_URL}/{ARTIFACTORY_RPM_REPO}"
+DEFAULT_ARTIFACTORY_USERNAME = "otel-collector"
+DEFAULT_TIMEOUT = 1200
+
+# Package/Release
+REPO_DIR = Path(__file__).parent.parent.parent.parent.resolve()
+ASSETS_BASE_DIR = REPO_DIR / "dist" / "release"
+COLLECTOR_REPO = "signalfx/splunk-otel-collector"
+EXTENSIONS = ["deb", "rpm", "exe", "msi"]
+PACKAGE_NAME = "splunk-otel-collector"
+STAGES = ("release", "beta", "test")
+S3_BUCKET = "public-downloads--signalfuse-com"
+S3_MSI_BASE_DIR = f"{PACKAGE_NAME}/msi"
+CLOUDFRONT_DISTRIBUTION_ID = "EJH671JAOI5SN"
+SMART_AGENT_RELEASE_PATH = REPO_DIR / "packaging" / "smart-agent-release.txt"
+
+# MSI
+WIX_IMAGE = "quay.io/signalfx/wix-dev:latest"
+WXS_PATH = "packaging/msi/splunk-otel-collector.wxs"
+MSI_CONFIG = "cmd/otelcol/config/collector/agent_config.yaml"
+FLUENTD_CONFIG = "packaging/fpm/etc/otel/collector/fluentd/fluent.conf"
+FLUENTD_CONFD = "packaging/msi/fluentd/conf.d"
+
+# Installer scripts
+INSTALLER_SCRIPTS_DIR = REPO_DIR / "packaging" / "installer"
+INSTALLER_SCRIPTS = {
+    "splunk-otel-collector.sh": INSTALLER_SCRIPTS_DIR / "install.sh",
+    "splunk-otel-collector.ps1": INSTALLER_SCRIPTS_DIR / "install.ps1",
+}

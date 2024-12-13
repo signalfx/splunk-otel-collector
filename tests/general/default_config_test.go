1c4a09a04113c131be1b9733a92d6cab9d79ceb5
Jina Jain <jjain@splunk.com>
2024-12-13 07:06:17 -0800

    Update tests/general/default_config_test.go

--- tests/general/default_config_test.go
+++ tests/general/default_config_test.go
@@ -122,7 +122,7 @@ func TestDefaultGatewayConfig(t *testing.T) {
 						"attributes": map[string]any{
 							"action": "insert",
 							"value":  "gateway",
-							"key":    "otelcol.service.node",
+							"key":    "otelcol.service.mode",
 						},
 					},
 				},

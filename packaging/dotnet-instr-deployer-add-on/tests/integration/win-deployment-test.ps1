36ba2c1c8f7b0fd291a4aa92cec601572921ba8d
Paulo Janotti <pjanotti@splunk.com>
2025-03-08 11:33:09 -0800

    Fix local/inputs destination name

--- packaging/dotnet-instr-deployer-add-on/tests/integration/win-deployment-test.ps1
+++ packaging/dotnet-instr-deployer-add-on/tests/integration/win-deployment-test.ps1
@@ -86,7 +86,7 @@ if (-not (Test-Path $localDir)) {
 # Copy the default inputs.conf to the local directory
 $defaultInputsConfPath = Join-Path $taPath "default/inputs.conf"
 if (Test-Path $defaultInputsConfPath) {
-    Copy-Item -Path $defaultInputsConfPath -Destination $inputsConfPath
+    Copy-Item -Path $defaultInputsConfPath -Destination $localInputsConfPath
     Write-Host "Copied default inputs.conf to local directory"
 } else {
     Write-Error "Default inputs.conf does not exist at $defaultInputsConfPath"
@@ -95,9 +95,6 @@ if (Test-Path $defaultInputsConfPath) {
 
 # Modify the local inputs.conf to uninstall the Splunk OpenTelemetry .NET instrumentation
 $localInputsConfContent = $(Get-Content $localInputsConfPath).Replace("uninstall = false", "uninstall = true")
-if (-not (Test-Path $localInputsConfPath)) {
-    New-Item -ItemType File -Force -Path $localInputsConfPath | Out-Null
-}
 Set-Content -Path $localInputsConfPath -Value $localInputsConfContent
 
 # Restart Splunk

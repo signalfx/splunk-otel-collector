19abdadab6ba9fd7354c85c418b206d38b6166c3
jvoravong <47871238+jvoravong@users.noreply.github.com>
2025-03-12 09:21:39 -0600

    Prepare TA release v1.4.1 (#5972)
    * Updates TA to splunk-otel-collector v and marks TA as v1.4.1
    
    * Update TA happypath-test.sh
    
    * Update release-ta-version.sh to support mac bash

--- packaging/technical-addon/packaging-scripts/release-ta-version.sh
+++ packaging/technical-addon/packaging-scripts/release-ta-version.sh
@@ -13,16 +13,28 @@ TAG="Splunk_TA_otel/v$TA_VERSION"
 
 git checkout -B "$BRANCH"
 git add "$SOURCE_DIR/Makefile" "$APP_CONFIG"
-git commit -m "Updates TA to splunk-otel-collector v$SPLUNK_OTEL_VERSION and marks TA as v$TA_VERSION" || echo "version changes already comitted"
-
+git commit -m "Updates TA to splunk-otel-collector v$SPLUNK_OTEL_VERSION and marks TA as v$TA_VERSION" || echo "version changes already committed"
 
 echo "Tagging..."
 git tag --annotate --sign --message "Release of Splunk_TA_otel version v$TA_VERSION" "$TAG"
 git push --set-upstream "$REMOTE" "$BRANCH"
-read -i "Do you want to push the tag to remote too? [y/N]" -r -n 1 USER_INPUT
-if [ "$USER_INPUT" == "y" ]; then
+
+PROMPT="Do you want to push the tag to remote too? [y/N] "
+USER_INPUT=""
+# Check if `read -i` is supported for GNU Bash, else use read command for MacOS Bash
+if (help read 2>&1 | grep -q -- "-i"); then
+    read -e -i "y" -p "$PROMPT" -r -n 1 USER_INPUT
+else
+    echo -n "$PROMPT"
+    read -r -n 1 USER_INPUT
+    echo ""
+fi
+
+# Convert input to lowercase and check
+if [[ "$USER_INPUT" == "y" ]]; then
     git push "$REMOTE" "$TAG"
     exit "$?"
 fi
-echo "To push this tag to remote, run "
-echo "git push origin '$TAG'"
+
+echo "Tag not pushed. To push this tag to remote, run "
+echo "git push $REMOTE '$TAG'"

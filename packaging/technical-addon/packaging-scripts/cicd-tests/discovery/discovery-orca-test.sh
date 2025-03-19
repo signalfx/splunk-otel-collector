adb4a8112cc1fb309db8d77937c40fcc56d7cb34
James Hughes <jameshughes@splunk.com>
2025-03-19 16:57:14 -0700

    wait derp not scp format for port

--- packaging/technical-addon/packaging-scripts/cicd-tests/discovery/discovery-orca-test.sh
+++ packaging/technical-addon/packaging-scripts/cicd-tests/discovery/discovery-orca-test.sh
@@ -94,7 +94,7 @@ for cmd in \
    "pgrep -f 'kafkametrics.discovery.properties.yaml'" \
    "test -d $SPLUNK_HOME/etc/apps/Splunk_TA_otel/configs/discovery/config.d.linux" \
 ; do
-    ssh -i ~/.orca/id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -P "$SSH_PORT" "$ORCA_SSH_USER@$IPV4_ADDR" "$cmd"
+    ssh -i ~/.orca/id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null  "$ORCA_SSH_USER@$IPV4_ADDR:$SSH_PORT" "$cmd"
 done
 
 

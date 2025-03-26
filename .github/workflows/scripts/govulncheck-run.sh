867a9771a88351182d6312b50f105676573c4d7a
Paulo Janotti <pjanotti@splunk.com>
2025-03-25 19:54:48 -0700

    Fail govulncheck if vulnerabilities are found

--- .github/workflows/scripts/govulncheck-run.sh
+++ .github/workflows/scripts/govulncheck-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -euo pipefail
+set -eo pipefail
 
 mkdir -p ./govulncheck 2>/dev/null
 
@@ -13,23 +13,34 @@ FAILED=0
 # Repository prefix to remove from package names
 REPO_PREFIX=$(go list -m)
 
+if [[ "$GOVULN_OPTS" =~ .*--format[[:space:]]+([a-z]+).* ]]; then
+  FORMAT=${BASH_REMATCH[1]}
+fi
+
 # Run govulncheck for each package
 for pkg in $ALL_PKG_DIRS; do
-  # Remove the repository prefix from the package name to keep the category names short
-  # and replace slashes with underscores to make clear that the categories are not nested.
-  OUTPUT_FILE="./govulncheck/$(echo "$pkg" | sed "s|^$REPO_PREFIX/||" | tr '/' '_').sarif"
-  echo -e "\nRunning govulncheck for package $pkg"
-  if ! govulncheck ${GOVULN_OPTS:-} "$pkg" > "$OUTPUT_FILE"; then
-    echo "govulncheck failed for package $pkg, output saved to $OUTPUT_FILE"
-    FAILED=1
+  echo -e "\n**** Running govulncheck for package $pkg"
+  set +e
+  if [[ -z $FORMAT ]]; then
+    govulncheck ${GOVULN_OPTS} $pkg
   else
-    echo "govulncheck succeeded for package $pkg, output saved to $OUTPUT_FILE"
+    # Remove the repository prefix from the package name to keep the category names short
+    # and replace slashes with underscores to make clear that the categories are not nested.
+    OUTPUT_FILE="./govulncheck/$(echo "$pkg" | sed "s|^$REPO_PREFIX/||" | tr '/' '_').$FORMAT"
+    govulncheck ${GOVULN_OPTS} $pkg > $OUTPUT_FILE
   fi
+  if [ $? -eq 0 ]; then
+    echo -e "\n**** govulncheck succeeded for package $pkg"
+  else
+    echo -e "\n**** govulncheck failed for package $pkg"
+    FAILED=1
+  fi
+  set -e
 done
 
 if [ $FAILED -ne 0 ]; then
-  echo -e "\ngovulncheck failed for one or more packages"
+  echo -e "\n**** govulncheck failed for one or more packages"
   exit 1
 fi
 
-echo -e "\ngovulncheck completed successfully for all packages"
+echo -e "\n**** govulncheck completed successfully for all packages"

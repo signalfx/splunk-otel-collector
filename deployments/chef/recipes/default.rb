40fd2692adb110bdf3d94306723d88540dce8f5b
Curtis Robert <crobert@splunk.com>
2025-06-02 11:00:19 -0700

    Update deployments/chef/recipes/default.rb

--- deployments/chef/recipes/default.rb
+++ deployments/chef/recipes/default.rb
@@ -12,7 +12,7 @@ if platform_family?('windows')
   include_recipe 'splunk_otel_collector::collector_win_config_options'
   include_recipe 'splunk_otel_collector::collector_win_install'
 
-  # Older MSI versions can't properly setup the collector configuration,
+  # Older MSI versions can't properly setup the collector configuration
   # in this case, we need to use the registry to set the environment variables.
   # Testing also showed custom configuration variables also require this.
   if !node['splunk_otel_collector']['collector_msi_is_configurable'] || !node['splunk_otel_collector']['collector_additional_env_vars'].empty?

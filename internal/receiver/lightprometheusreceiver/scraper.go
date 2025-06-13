091813cd92edc4d935938dc0e4943707a351722f
jvoravong <jvoravong@splunk.com>
2025-06-13 10:30:33 -0600

    light prometheus receiver "go.opentelemetry.io/otel/semconv/v1.22.0" -> "go.opentelemetry.io/otel/semconv/v1.25.0"

--- internal/receiver/lightprometheusreceiver/scraper.go
+++ internal/receiver/lightprometheusreceiver/scraper.go
@@ -29,7 +29,7 @@ import (
 	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/pdata/pmetric"
 	"go.opentelemetry.io/collector/receiver"
-	conventions "go.opentelemetry.io/otel/semconv/v1.22.0"
+	conventions "go.opentelemetry.io/otel/semconv/v1.25.0"
 	"go.uber.org/zap"
 )
 

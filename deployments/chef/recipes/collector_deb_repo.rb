efe8eaf77e8e3829b6abf807af7cdafec6b4c482
Curtis Robert <crobert@splunk.com>
2025-06-17 14:28:23 -0700

    [chore][chef] Run tests against locally built artifact

--- deployments/chef/recipes/collector_deb_repo.rb
+++ deployments/chef/recipes/collector_deb_repo.rb
@@ -7,10 +7,40 @@ remote_file '/etc/apt/trusted.gpg.d/splunk.gpg' do
   action :create
 end
 
-file '/etc/apt/sources.list.d/splunk-otel-collector.list' do
-  content "deb #{node['splunk_otel_collector']['debian_repo_url']} #{node['splunk_otel_collector']['package_stage']} main\n"
-  mode '0644'
-  notifies :update, 'apt_update[update apt cache]', :immediately
+if node['splunk_otel_collector']['local_artifact_testing_enabled']
+  cpu_arch = 'amd64'
+  if arm?
+    cpu_arch = 'arm64'
+  end
+
+  file_name = 'deb-' + cpu_arch + '-package'
+  deb_install_dir = '/etc/otel/collector/'
+  deb_install_path = deb_install_dir + file_name
+
+  # Create destination dir on target machine
+  directory deb_install_dir do
+    mode '0644'
+    action :create
+    recursive true
+  end
+
+  # Copy deb file from source to target
+  cookbook_file deb_install_path do
+    source file_name
+    mode '0644'
+  end
+
+  dpkg_package deb_install_path do
+    source deb_install_path
+    action :install
+  end
+
+else
+  file '/etc/apt/sources.list.d/splunk-otel-collector.list' do
+    content "deb #{node['splunk_otel_collector']['debian_repo_url']} #{node['splunk_otel_collector']['package_stage']} main\n"
+    mode '0644'
+    notifies :update, 'apt_update[update apt cache]', :immediately
+  end
 end
 
 apt_update 'update apt cache' do

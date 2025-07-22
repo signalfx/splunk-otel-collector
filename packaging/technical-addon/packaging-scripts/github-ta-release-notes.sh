ce8fcf07e395e1380c7b755352dc1d23f63e3738
James Hughes <jameshughes@splunk.com>
2025-07-21 17:14:08 -0700

    change release notes invocation

--- packaging/technical-addon/packaging-scripts/github-ta-release-notes.sh
+++ packaging/technical-addon/packaging-scripts/github-ta-release-notes.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -eux
 
-CHANGELOG="${REPO_DIR}/CHANGELOG.md"
+CHANGELOG="${ADDONS_SOURCE_DIR}/${TA_NAME}/CHANGELOG.md"
 
-changes="$( awk -v version="$VERSION" '/^## / { if (p) { exit }; if ($2 == version) { p=1; next } } p && NF' "$CHANGELOG" )"
+changes="$( awk -v version="$TA_VERSION" '/^## / { if (p) { exit }; if ($2 == version) { p=1; next } } p && NF' "$CHANGELOG" )"
 
 if [[ -z "$changes" ]] || [[ "$changes" =~ ^[[:space:]]+$ ]]; then
   changes="Release notes in progress."

8fc0919c6e8afc61f6f1d841dee01ac8f2c47525
Paulo Janotti <pjanotti@splunk.com>
2025-08-10 18:40:27 -0700

    Refactor of ps script

--- packaging/technical-addon/Splunk_TA_otel/windows_x86_64/bin/Splunk_TA_otel_utils.ps1
+++ packaging/technical-addon/Splunk_TA_otel/windows_x86_64/bin/Splunk_TA_otel_utils.ps1
@@ -1,85 +1,88 @@
 $otelProcessName=$args[0]
 
-function otelLogWrite
-{
-   Param ([string]$logstring)
-   $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss.fff K"
+$scriptName = $MyInvocation.MyCommand.Name
 
-   # Write-Warning is much less verbose than Write-Error and the other Write-* may end up
-   # not being captured by the parent process (expected to be the cmd that launched the script)
-   Write-Warning "$timestamp Splunk_TA_otel_utils.ps1: $logstring"
+function Write-Log
+{
+	Param ([string]$msg)
+	$timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss.fff K"
+
+	# Write-Warning is much less verbose than Write-Error and the other Write-* may end up
+	# not being captured by the parent process (expected to be the cmd that launched the script)
+	Write-Warning "${scriptName}: $timestamp $msg"
 }
 
-function getParentProcessId($id)
+function Get-Win32_ProcessByNameAndParentId
 {
-	return (Get-WmiObject Win32_Process | Where-Object ProcessId -eq $id).ParentProcessId
+	Param (
+		[string]$processName,
+		[Int32]$parentId,
+		[TimeSpan]$timeout,
+		[TimeSpan]$sleepInterval)
+
+	$process = $null
+	$startTime = Get-Date
+
+	do {
+		$process = Get-WmiObject Win32_Process | Where-Object ParentProcessId -eq $parentId | Where-Object ProcessName -eq $processName
+		if ($process) {
+			break
+		}
+
+		Start-Sleep -Milliseconds ($sleepInterval.TotalMilliseconds)
+	} while (((Get-Date) - $startTime) -lt $timeout)
+
+	return $process
 }
 
-function getParentAndGrandParent($id)
+function Stop-ProcessById
 {
-	try {
-		$parentId = getParentProcessId($id)
-		otelLogWrite "INFO Parent process id: $parentId"
-		$grandParentId = getParentProcessId($parentId)
-		otelLogWrite "INFO GrandParent process id: $grandParentId"		
-	}
-	catch {
-		# Use Write-Error directly here to get detailed information.
-		Write-Error "ERROR getParentAndGrandParent: $_"
-	}
+	Param ([Int32]$processId)
 
-	return $parentId, $grandParentId
-}
-
-function getOtelProcessPid($processName, $parentId)
-{
-	otelLogWrite "INFO Getting PID for $processName with parent PID $parentId ..."
-	$procPid = (Get-WmiObject Win32_Process | Where-Object ParentProcessId -eq $parentId | Where-Object ProcessName -eq $processName).ProcessId
-	otelLogWrite "INFO Collector process id: $procPid"
-	$otelProcess = Get-Process -Id $procPid -ErrorAction Ignore
-	if($otelProcess)
-	{
-		return $procPid
-	}
-	else
-	{
-		return -1
-	}
-}
-
-function forceStopProcess($processId)
-{
-	otelLogWrite "INFO Stopping $processId if stil running"
+	Write-Log "INFO Stopping PID $processId if still running"
 	# Use a pipeline to avoid an error message if the process already finished
 	Get-WmiObject Win32_Process | Where-Object ProcessId -eq $processId | ForEach-Object { taskkill.exe /f /pid $_.ProcessId }
 }
 
-otelLogWrite "INFO Splunk_TA_otelutils.ps1 started"
+function Get-Win32_Process {
+	Param([Int32]$processId)
+	return Get-WmiObject Win32_Process -Filter "ProcessId = $processId"
+}
+
+Write-Log "INFO started"
 try {
-	otelLogWrite "INFO sleeping ..."
-	start-sleep -s 3
-	otelLogWrite "INFO getting parent and grand parent PIDs"
-	$parentId, $grandParentId = getParentAndGrandParent $PID
-	otelLogWrite "INFO got parent $parentId and grand parent $grandParentId PIDs"
-	$otelPid = getOtelProcessPid $otelProcessName $parentId
-	if ($otelPid -eq -1) {
-		otelLogWrite "ERROR Otel agent not running"
-	} else {
-		otelLogWrite "INFO Otel agent running"
-		$parentJob      = Start-Job { Wait-Process -Id $using:parentId } -Name "cmd job"
-		$grandParentJob = Start-Job { Wait-Process -Id $using:grandParentId } -Name "splunkd job"
-		$otelJob        = Start-Job { Wait-Process -Id $using:otelPid } -Name "Otel agent job"
-		otelLogWrite "INFO waiting on termination of any one of splunkd, cmd, and Otel agent"
-		$finishedJob = Wait-Job -Any -Job $parentJob,$grandParentJob,$otelJob
-		otelLogWrite "INFO at least one of the jobs finished:`n`t'$($finishedJob.Name)': finished with $($finishedJob.JobStateInfo)"
-		forceStopProcess $otelPid
-		forceStopProcess $parentId
-	}	
+	$currentProcess     = Get-Win32_Process $PID # $PID is predefined as the PID of the current process
+	$parentProcess      = Get-Win32_Process $currentProcess.ParentProcessId
+	$grandParentProcess = Get-Win32_Process $parentProcess.ParentProcessId
+
+	# splunkd launched a cmd that launched the collector, but didn't wait on it and then launched this script
+	# get the expected process for the OTel collector launched via splunkd
+	$otelProcess = Get-Win32_ProcessByNameAndParentId `
+		$otelProcessName `
+		$parentProcess.ProcessId `
+		-timeout ([TimeSpan]::FromSeconds(5)) `
+		-sleepInterval ([TimeSpan]::FromMilliseconds(150))
+	if ($null -eq $otelProcess) {
+		Write-Log "ERROR Otel agent not running"
+		return
+	}
+
+	Write-Log "INFO Otel agent running (PID $($otelProcess.ProcessId))"
+	$parentJob      = Start-Job { Wait-Process -Id $using:parentProcess.ProcessId }      -Name "cmd job"
+	$grandParentJob = Start-Job { Wait-Process -Id $using:grandParentProcess.ProcessId } -Name "splunkd job"
+	$otelJob        = Start-Job { Wait-Process -Id $using:otelProcess.ProcessId }        -Name "Otel agent job"
+
+	Write-Log "INFO waiting on termination of splunkd, cmd, or the Otel agent"
+	$finishedJob = Wait-Job -Any -Job $parentJob,$grandParentJob,$otelJob
+
+	Write-Log "INFO at least one of the jobs finished:`n`t'$($finishedJob.Name)': finished with $($finishedJob.JobStateInfo)"
+	Stop-ProcessById $otelProcess.ProcessId
+	Stop-ProcessById $parentProcess.ProcessId
 }
 catch {
 	# Use Write-Error directly here to get detailed information.
 	Write-Error "ERROR $_"
 }
 finally {
-	otelLogWrite "INFO finished"
+	Write-Log "INFO finished"
 }

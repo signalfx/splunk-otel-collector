c644070235a46f38a4af27e3762a7210dd90574d
Dani Louca <dlouca@splunk.com>
2025-08-12 13:25:50 -0400

    [chore] Skip Changelog for TA release branch
    Signed-off-by: Dani Louca <dlouca@splunk.com>

--- packaging/technical-addon/packaging-scripts/release-ta-version.sh
+++ packaging/technical-addon/packaging-scripts/release-ta-version.sh
@@ -13,7 +13,7 @@ TAG="Splunk_TA_otel/v$TA_VERSION"
 
 git checkout -B "$BRANCH"
 git add "$ADDONS_SOURCE_DIR/Makefile" "$APP_CONFIG" "$ADDONS_SOURCE_DIR/Splunk_TA_otel/CHANGELOG.md" "$ADDONS_SOURCE_DIR/packaging-scripts/cicd-tests/happypath-test.sh"
-git commit -m "Updates TA to splunk-otel-collector v$SPLUNK_OTEL_VERSION and marks TA as v$TA_VERSION" || echo "version changes already committed"
+git commit -m "[chore] Updates TA to splunk-otel-collector v$SPLUNK_OTEL_VERSION and marks TA as v$TA_VERSION" || echo "version changes already committed"
 
 git push --set-upstream "$REMOTE" "$BRANCH"
 git push "$REMOTE" "$BRANCH"

fcc46e245fece0901849ff55a8baf2bb4b78fef4
Paulo Janotti <pjanotti@splunk.com>
2025-09-10 20:17:13 -0700

    Try to add debug statements

--- packaging/technical-addon/packaging-scripts/cicd-tests/smoketests/smoketest.sh
+++ packaging/technical-addon/packaging-scripts/cicd-tests/smoketests/smoketest.sh
@@ -7,6 +7,8 @@ set -o pipefail
 source "${ADDONS_SOURCE_DIR}/packaging-scripts/cicd-tests/test-utils.sh"
 TA_FULLPATH="$(repack_with_test_config "foobar" "$BUILD_DIR/out/distribution/Splunk_TA_otel.tgz" | tail -n 1)"
 # Dump the repacked local yaml files for debugging
+echo "Listing repacked files"
+find "$BUILD_DIR/repack/" -name "*"
 for yaml_file in "$BUILD_DIR/repack/Splunk_TA_otel/local/"*.yaml; do
     if [ -f "$yaml_file" ]; then
         echo -e "\n=============== $yaml_file =================="

86bec8d95ab0afa049bfa80d19ce847af630e065
Paulo Janotti <pjanotti@splunk.com>
2025-09-22 16:30:23 -0700

    Move .NET in test-package to v 8.0

--- packaging/tests/instrumentation/setup-dotnet.sh
+++ packaging/tests/instrumentation/setup-dotnet.sh
@@ -8,16 +8,17 @@ DOTNET_APP_HOME=/opt/dotnet
 
 useradd -r -m -U -d $DOTNET_APP_HOME -s /bin/false dotnet
 
-wget -nv https://download.visualstudio.microsoft.com/download/pr/19144d78-6f95-4810-a9f6-3bf86035a244/23f4654fc5352e049b517937f94be839/dotnet-sdk-6.0.421-linux-x64.tar.gz -O dotnet-sdk.tar.gz
+wget -nv https://builds.dotnet.microsoft.com/dotnet/Runtime/8.0.20/dotnet-runtime-8.0.20-linux-x64.tar.gz -O dotnet-sdk.tar.gz
 mkdir -p $DOTNET_SDK_HOME
 tar -xzf dotnet-sdk.tar.gz -C $DOTNET_SDK_HOME
 rm -f dotnet-sdk.tar.gz
 chmod a+x $DOTNET_BIN
 
-wget -nv https://github.com/docker/docker-dotnet-sample/archive/c09abb8c745f336312049db50fcebaefa5a1764a.tar.gz -O docker-dotnet-sample.tar.gz
+# The SHA below is the commit targeted for the sample application
+wget -nv https://github.com/docker/docker-dotnet-sample/archive/c7f01a5a7f2058bc1e1e29f8cfdb92fd1800054d.tar.gz -O docker-dotnet-sample.tar.gz
 tar -xzf docker-dotnet-sample.tar.gz -C /tmp
 rm -f docker-dotnet-sample.tar.gz
-$DOTNET_BIN publish /tmp/docker-dotnet-sample-c09abb8c745f336312049db50fcebaefa5a1764a/src -a x64 -o $DOTNET_APP_HOME
+$DOTNET_BIN publish /tmp/docker-dotnet-sample-c7f01a5a7f2058bc1e1e29f8cfdb92fd1800054d/src -a x64 -o $DOTNET_APP_HOME
 chown -R dotnet:dotnet $DOTNET_APP_HOME
 
 mkdir -p /etc/systemd/system

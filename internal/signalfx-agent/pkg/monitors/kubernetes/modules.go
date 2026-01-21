1d7bb76b368516a2b9aebc285afa615fc5ee1916
Antoine Toulme <atoulme@splunk.com>
2026-01-20 21:41:25 -0800

    [kubelet-metrics] remove plugin (#7081)

--- internal/signalfx-agent/pkg/monitors/kubernetes/modules.go
+++ internal/signalfx-agent/pkg/monitors/kubernetes/modules.go
@@ -6,7 +6,6 @@ import (
 	_ "github.com/signalfx/signalfx-agent/pkg/monitors/kubernetes/cluster"
 	_ "github.com/signalfx/signalfx-agent/pkg/monitors/kubernetes/controllermanager"
 	_ "github.com/signalfx/signalfx-agent/pkg/monitors/kubernetes/events"
-	_ "github.com/signalfx/signalfx-agent/pkg/monitors/kubernetes/kubeletmetrics"
 	_ "github.com/signalfx/signalfx-agent/pkg/monitors/kubernetes/proxy"
 	_ "github.com/signalfx/signalfx-agent/pkg/monitors/kubernetes/scheduler"
 	_ "github.com/signalfx/signalfx-agent/pkg/monitors/kubernetes/volumes"

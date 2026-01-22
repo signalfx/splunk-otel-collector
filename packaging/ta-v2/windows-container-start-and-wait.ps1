6bc15652ef63eeca90f37bd9de40467a58f4f891
Paulo Janotti <pjanotti@splunk.com>
2026-01-21 19:04:45 -0800

    Add a more detailed start script for Windows container

---
+++ packaging/ta-v2/windows-container-start-and-wait.ps1
@@ -0,0 +1,20 @@
+$splunkdLog = "C:/Program Files/SplunkUniversalForwarder/var/log/splunk/splunkd.log"
+
+Get-Service SplunkForwarder
+
+Write-Host "Waiting for splunkd.log ..."
+$timeout = 60
+$elapsed = 0
+while (-not (Test-Path -Path $splunkdLog)) {
+    $elapsed += 2
+    if ($elapsed -ge $timeout) {
+        Write-Host -NoNewline "."
+        Write-Host "Timeout: splunkd.log was not created within $timeout seconds" -ForegroundColor Red
+        exit 1
+    }
+    Start-Sleep -Seconds 2
+}
+
+Get-Service SplunkForwarder
+
+Get-Content -Path $splunkdLog -Wait

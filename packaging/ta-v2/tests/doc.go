0b9129472ab123da907f669bcbc87fa36ffa5301
Paulo Janotti <pjanotti@splunk.com>
2026-01-21 16:35:09 -0800

    Fix global golang targets

---
+++ packaging/ta-v2/tests/doc.go
@@ -0,0 +1,18 @@
+// Copyright  Splunk, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package packaing/ta-v2/tests provides tests and utilities for verifying
+// the Splunk TA for OpenTelemetry Collector v2 package.
+// This package is being created only to satisfy the Go toolchain.
+package tests

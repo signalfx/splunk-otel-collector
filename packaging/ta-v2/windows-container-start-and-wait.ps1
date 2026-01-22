2aa05766568c90975b1b895ede17943a8de12a4a
Paulo Janotti <pjanotti@splunk.com>
2026-01-22 07:18:20 -0800

    Revert "Try installing Splunk when starting the container"
    This reverts commit 271823b9aa086fb63ba6607cdc6d76a9ced81c24.

--- packaging/ta-v2/windows-container-start-and-wait.ps1
+++ packaging/ta-v2/windows-container-start-and-wait.ps1
@@ -1,11 +1,3 @@
-Start-Service msiserver
-msiexec.exe /i C:\splunk-uf.msi /qn /norestart /l*v C:\splunk-uf-install.log AGREETOLICENSE=Yes
-if ($LASTEXITCODE -ne 0) {
-    Write-Host "Error: Splunk Universal Forwarder installation failed with exit code $LASTEXITCODE" -ForegroundColor Red
-    Get-Content C:/splunk-uf-install.log
-    exit 1
-}
-
 $splunkdLog = "C:/Program Files/SplunkUniversalForwarder/var/log/splunk/splunkd.log"
 
 Get-Content C:/splunk-uf-install.log

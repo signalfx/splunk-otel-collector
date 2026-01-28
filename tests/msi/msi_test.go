13df598553b9c84670d645204bfb9fc887e033ac
Paulo Janotti <pjanotti@splunk.com>
2026-01-27 17:49:14 -0800

    Use a tag to identify tests that require the MSI

--- tests/msi/msi_test.go
+++ tests/msi/msi_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build windows && !arm64
+//go:build windows && msi
 
 package msi
 

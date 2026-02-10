6f68d533d0e088245b65854c16e36bd78055703e
Paulo Janotti <pjanotti@splunk.com>
2026-02-10 09:02:54 -0800

    Generate updated expected files in case of failure

--- tests/zeroconfig/windows/windows_iis_test.go
+++ tests/zeroconfig/windows/windows_iis_test.go
@@ -17,8 +17,10 @@
 package zeroconfig
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
 	"os/exec"
 	"path"
 	"path/filepath"
@@ -28,6 +30,8 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/collector/pdata/ptrace"
+	"go.yaml.in/yaml/v3"
 
 	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/golden"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/pdatatest/ptracetest"
@@ -127,11 +131,16 @@ func testExpectedTracesForHTTPGetRequest(t *testing.T, otlp *testutils.OTLPRecei
 	}, 3*time.Minute, 100*time.Millisecond, "Failed to connect to target")
 
 	var index int
+	var marshaledTrace []byte
 	assert.EventuallyWithT(t, func(c *assert.CollectT) {
 		matchErr := fmt.Errorf("no matching traces found, %d collected", index)
 		newIndex := len(otlp.AllTraces())
 		for i := index; i < newIndex && matchErr != nil; i++ {
-			matchErr = ptracetest.CompareTraces(expected, otlp.AllTraces()[i],
+			trace := otlp.AllTraces()[i]
+			marshaler := ptrace.JSONMarshaler{}
+			marshaledTrace, err = marshaler.MarshalTraces(trace)
+			require.NoError(c, err)
+			matchErr = ptracetest.CompareTraces(expected, trace,
 				ptracetest.IgnoreResourceAttributeValue("host.id"),
 				ptracetest.IgnoreResourceAttributeValue("host.name"),
 				ptracetest.IgnoreResourceAttributeValue("host.arch"),
@@ -156,5 +165,19 @@ func testExpectedTracesForHTTPGetRequest(t *testing.T, otlp *testutils.OTLPRecei
 		}
 		index = newIndex
 		assert.NoError(c, matchErr)
-	}, 1*time.Minute, 10*time.Millisecond, "Failed to receive expected traces")
+	}, 1*time.Minute, 100*time.Millisecond, "Failed to receive expected traces")
+
+	if t.Failed() {
+		t.Logf("Expected traces did not match actual traces.")
+		if marshaledTrace != nil {
+			t.Logf("Writing received traces over expected traces file: %s", expectedTracesFileName)
+
+			var data interface{}
+			errUnmarshal := json.Unmarshal(marshaledTrace, &data)
+			require.NoError(t, errUnmarshal)
+			yamlBytes, errYAMLMarshal := yaml.Marshal(data)
+			require.NoError(t, errYAMLMarshal)
+			require.NoError(t, os.WriteFile(expectedTracesFileName, yamlBytes, 0644))
+		}
+	}
 }

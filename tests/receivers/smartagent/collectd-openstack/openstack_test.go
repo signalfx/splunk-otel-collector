627e274ddd41849ef7ae08ab064d37765957321d
Paulo Janotti <pjanotti@splunk.com>
2026-04-17 11:30:34 -0700

    Coalesce tests into a single fund

--- tests/receivers/smartagent/collectd-openstack/openstack_test.go
+++ tests/receivers/smartagent/collectd-openstack/openstack_test.go
@@ -32,6 +32,101 @@ import (
 	"github.com/signalfx/splunk-otel-collector/tests/testutils"
 )
 
+// TestCollectdOpenstackReceiver tests that the collectd/openstack monitor emits the expected
+// metrics when connected to a live devstack OpenStack deployment running on the host machine
+// (deployed via gophercloud/devstack-action in CI).
+func TestCollectdOpenstackReceiver(t *testing.T) {
+	// ATTENTION: currently Cinder metrics are NOT being tested since it seems that the monitor code is too old
+	// and has not been updated since Cinder switched to block-storage vs volumev2 service type. This needs
+	// to be investigated as a separate issue. For now, tests of "all" and "default" metrics do not include
+	// Cinder metrics.
+	testCases := []struct {
+		name       string
+		configFile string
+		expected   string
+	}{
+		{
+			name:       "DefaultMetrics",
+			configFile: "default_metrics_config.yaml",
+			expected:   "default.yaml",
+		},
+		{
+			name:       "AllMetrics",
+			configFile: "all_metrics_config.yaml",
+			expected:   "all.yaml",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			testcase := testutils.NewTestcase(t)
+			defer testcase.PrintLogsOnFailure()
+			defer testcase.ShutdownOTLPReceiverSink()
+
+			_, shutdown := testcase.SplunkOtelCollectorProcess(tc.configFile)
+			defer shutdown()
+
+			expected, err := golden.ReadMetrics(filepath.Join("testdata", "expected", tc.expected))
+			require.NoError(t, err)
+
+			lastIndex := 0
+			ok := assert.EventuallyWithT(t, func(tt *assert.CollectT) {
+				if len(testcase.OTLPReceiverSink.AllMetrics()) == 0 {
+					assert.Fail(tt, "No metrics collected")
+					return
+				}
+				cmpErr := errors.New("no comparison was made")
+				newIndex := len(testcase.OTLPReceiverSink.AllMetrics()) - 1
+				for i := newIndex; i >= lastIndex; i-- {
+					m := testcase.OTLPReceiverSink.AllMetrics()[i]
+					if m.MetricCount() >= expected.MetricCount() {
+						cmpErr = pmetrictest.CompareMetrics(expected, m,
+							pmetrictest.IgnoreMetricAttributeValue("host"),
+							pmetrictest.IgnoreMetricAttributeValue("hypervisor_hostname"),
+							pmetrictest.IgnoreMetricAttributeValue("plugin_instance"),
+							pmetrictest.IgnoreMetricAttributeValue("project_id"),
+							pmetrictest.IgnoreMetricsOrder(),
+							pmetrictest.IgnoreMetricValues(),
+							pmetrictest.IgnoreTimestamp(),
+							pmetrictest.IgnoreStartTimestamp(),
+							pmetrictest.IgnoreMetricDataPointsOrder(),
+							pmetrictest.IgnoreScopeMetricsOrder(),
+							pmetrictest.IgnoreResourceMetricsOrder(),
+							pmetrictest.IgnoreScopeVersion(),
+							pmetrictest.IgnoreSubsequentDataPoints(),
+						)
+						if cmpErr == nil {
+							return
+						}
+						t.Logf("Batch %d had %d metrics but did not match expected: %v", i, m.MetricCount(), cmpErr)
+						filename := fmt.Sprintf("testdata/received_failed_batch_%s.yaml", time.Now().Format("20060102_150405"))
+						if err := golden.WriteMetricsToFile(filename, m); err != nil {
+							t.Logf("dumpBatchReceivedMetrics: failed to write %s: %v", filename, err)
+						}
+					}
+				}
+				lastIndex = newIndex
+				assert.NoError(tt, cmpErr)
+			}, 2*time.Minute, 5*time.Second)
+
+			if !ok {
+				t.Logf("expected %d metrics, largest batch had %d", expected.MetricCount(), largestBatchMetricCount(testcase))
+				dumpReceivedMetrics(t, testcase, fmt.Sprintf("testdata/received_%s.yaml", t.Name()))
+			}
+		})
+	}
+}
+
+func largestBatchMetricCount(tc *testutils.Testcase) int {
+	max := 0
+	for _, m := range tc.OTLPReceiverSink.AllMetrics() {
+		if c := m.MetricCount(); c > max {
+			max = c
+		}
+	}
+	return max
+}
+
 // dumpReceivedMetrics writes all metrics received during a test to a YAML file for debugging.
 func dumpReceivedMetrics(t *testing.T, tc *testutils.Testcase, filename string) {
 	t.Helper()
@@ -49,130 +144,4 @@ func dumpReceivedMetrics(t *testing.T, tc *testutils.Testcase, filename string) 
 	if err := golden.WriteMetricsToFile(filename, merged); err != nil {
 		t.Logf("dumpReceivedMetrics: failed to write %s: %v", filename, err)
 	}
-}
-
-// TestCollectdOpenstackReceiverProvidesDefaultMetrics tests that the collectd/openstack monitor
-// emits all default metrics when connected to a live devstack OpenStack deployment running on
-// the host machine (deployed via gophercloud/devstack-action in CI).
-func TestCollectdOpenstackReceiverProvidesDefaultMetrics(t *testing.T) {
-	tc := testutils.NewTestcase(t)
-	defer tc.PrintLogsOnFailure()
-	defer tc.ShutdownOTLPReceiverSink()
-
-	_, shutdown := tc.SplunkOtelCollectorProcess("default_metrics_config.yaml")
-	defer shutdown()
-
-	expected, err := golden.ReadMetrics(filepath.Join("testdata", "expected", "default.yaml"))
-	require.NoError(t, err)
-
-	lastIndex := 0
-	ok := assert.EventuallyWithT(t, func(tt *assert.CollectT) {
-		if len(tc.OTLPReceiverSink.AllMetrics()) == 0 {
-			assert.Fail(tt, "No metrics collected")
-			return
-		}
-		cmpErr := errors.New("no comparison was made")
-		newIndex := len(tc.OTLPReceiverSink.AllMetrics()) - 1
-		for i := newIndex; i >= lastIndex; i-- {
-			m := tc.OTLPReceiverSink.AllMetrics()[i]
-			if m.MetricCount() >= expected.MetricCount() {
-				cmpErr = pmetrictest.CompareMetrics(expected, m,
-					pmetrictest.IgnoreMetricAttributeValue("host"),
-					pmetrictest.IgnoreMetricAttributeValue("hypervisor_hostname"),
-					pmetrictest.IgnoreMetricAttributeValue("plugin_instance"),
-					pmetrictest.IgnoreMetricAttributeValue("project_id"),
-					pmetrictest.IgnoreMetricsOrder(),
-					pmetrictest.IgnoreMetricValues(),
-					pmetrictest.IgnoreTimestamp(),
-					pmetrictest.IgnoreStartTimestamp(),
-					pmetrictest.IgnoreMetricDataPointsOrder(),
-					pmetrictest.IgnoreScopeMetricsOrder(),
-					pmetrictest.IgnoreResourceMetricsOrder(),
-					pmetrictest.IgnoreScopeVersion(),
-					pmetrictest.IgnoreSubsequentDataPoints(),
-				)
-				if cmpErr == nil {
-					return
-				}
-				t.Logf("Batch %d had %d metrics but did not match expected: %v", i, m.MetricCount(), cmpErr)
-				filename := fmt.Sprintf("testdata/received_failed_batch_%s.yaml", time.Now().Format("20060102_150405"))
-				if err := golden.WriteMetricsToFile(filename, m); err != nil {
-					t.Logf("dumpBatchReceivedMetrics: failed to write %s: %v", filename, err)
-				}
-			}
-		}
-		lastIndex = newIndex
-		assert.NoError(tt, cmpErr)
-	}, 2*time.Minute, 5*time.Second)
-
-	if !ok {
-		t.Logf("expected %d metrics, largest batch had %d", expected.MetricCount(), largestBatchMetricCount(tc))
-		dumpReceivedMetrics(t, tc, fmt.Sprintf("testdata/received_%s.yaml", t.Name()))
-	}
-}
-
-// TestCollectdOpenstackReceiverProvidesAllMetrics tests that the collectd/openstack monitor
-// emits all metrics (including non-default ones) when extraMetrics is configured with a wildcard.
-// Requires a live devstack OpenStack deployment running on the host machine.
-func TestCollectdOpenstackReceiverProvidesAllMetrics(t *testing.T) {
-	tc := testutils.NewTestcase(t)
-	defer tc.PrintLogsOnFailure()
-	defer tc.ShutdownOTLPReceiverSink()
-
-	_, shutdown := tc.SplunkOtelCollectorProcess("all_metrics_config.yaml")
-	defer shutdown()
-
-	expected, err := golden.ReadMetrics(filepath.Join("testdata", "expected", "all.yaml"))
-	require.NoError(t, err)
-
-	lastIndex := 0
-	ok := assert.EventuallyWithT(t, func(tt *assert.CollectT) {
-		if len(tc.OTLPReceiverSink.AllMetrics()) == 0 {
-			assert.Fail(tt, "No metrics collected")
-			return
-		}
-		cmpErr := errors.New("no comparison was made")
-		newIndex := len(tc.OTLPReceiverSink.AllMetrics()) - 1
-		for i := newIndex; i >= lastIndex; i-- {
-			m := tc.OTLPReceiverSink.AllMetrics()[i]
-			if m.MetricCount() >= expected.MetricCount() {
-				cmpErr = pmetrictest.CompareMetrics(expected, m,
-					pmetrictest.IgnoreMetricAttributeValue("host"),
-					pmetrictest.IgnoreMetricAttributeValue("hypervisor_hostname"),
-					pmetrictest.IgnoreMetricAttributeValue("plugin_instance"),
-					pmetrictest.IgnoreMetricAttributeValue("project_id"),
-					pmetrictest.IgnoreMetricsOrder(),
-					pmetrictest.IgnoreMetricValues(),
-					pmetrictest.IgnoreTimestamp(),
-					pmetrictest.IgnoreStartTimestamp(),
-					pmetrictest.IgnoreMetricDataPointsOrder(),
-					pmetrictest.IgnoreScopeMetricsOrder(),
-					pmetrictest.IgnoreResourceMetricsOrder(),
-					pmetrictest.IgnoreScopeVersion(),
-					pmetrictest.IgnoreSubsequentDataPoints(),
-				)
-				if cmpErr == nil {
-					return
-				}
-				t.Logf("Batch %d had %d metrics but did not match expected: %v", i, m.MetricCount(), cmpErr)
-			}
-		}
-		lastIndex = newIndex
-		assert.NoError(tt, cmpErr)
-	}, 2*time.Minute, 5*time.Second)
-
-	if !ok {
-		t.Logf("expected %d metrics, largest batch had %d", expected.MetricCount(), largestBatchMetricCount(tc))
-		dumpReceivedMetrics(t, tc, fmt.Sprintf("testdata/received_%s.yaml", t.Name()))
-	}
-}
-
-func largestBatchMetricCount(tc *testutils.Testcase) int {
-	max := 0
-	for _, m := range tc.OTLPReceiverSink.AllMetrics() {
-		if c := m.MetricCount(); c > max {
-			max = c
-		}
-	}
-	return max
 }

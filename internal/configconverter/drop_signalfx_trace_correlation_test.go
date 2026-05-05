78324ab747def23d36add3005ca8a3fb9276bb1f
crobert-1 <crobert@splunk.com>
2026-05-05 13:45:49 -0700

    add test case for dropping an entire pipeline from valid config, reorder and clean up tests

--- internal/configconverter/drop_signalfx_trace_correlation_test.go
+++ internal/configconverter/drop_signalfx_trace_correlation_test.go
@@ -23,217 +23,6 @@ import (
 	"go.opentelemetry.io/collector/featuregate"
 )
 
-func TestDropSignalFxTracesExporterIfFeatureGateEnabled(t *testing.T) {
-	tests := []struct {
-		name               string
-		input              string
-		expected           string
-		featureGateEnabled bool
-	}{
-		{
-			name:               "feature_gate_enabled",
-			input:              "testdata/drop_signalfx_exporter_traces_pipelines/input_config.yaml",
-			expected:           "testdata/drop_signalfx_exporter_traces_pipelines/expected_config.yaml",
-			featureGateEnabled: true,
-		},
-		{
-			name:               "feature_gate_disabled",
-			input:              "testdata/drop_signalfx_exporter_traces_pipelines/input_config.yaml",
-			expected:           "testdata/drop_signalfx_exporter_traces_pipelines/input_config.yaml",
-			featureGateEnabled: false,
-		},
-		{
-			name:               "agent_config_feature_gate_enabled",
-			input:              "../../cmd/otelcol/config/collector/agent_config.yaml",
-			expected:           "testdata/drop_signalfx_exporter_traces_pipelines/agent_config_expected.yaml",
-			featureGateEnabled: true,
-		},
-		{
-			name:               "agent_config_feature_gate_disabled",
-			input:              "../../cmd/otelcol/config/collector/agent_config.yaml",
-			expected:           "../../cmd/otelcol/config/collector/agent_config.yaml",
-			featureGateEnabled: false,
-		},
-		{
-			name:               "ecs_ec2_config_feature_gate_enabled",
-			input:              "../../cmd/otelcol/config/collector/ecs_ec2_config.yaml",
-			expected:           "testdata/drop_signalfx_exporter_traces_pipelines/ecs_ec2_config_expected.yaml",
-			featureGateEnabled: true,
-		},
-		{
-			name:               "ecs_ec2_config_feature_gate_disabled",
-			input:              "../../cmd/otelcol/config/collector/ecs_ec2_config.yaml",
-			expected:           "../../cmd/otelcol/config/collector/ecs_ec2_config.yaml",
-			featureGateEnabled: false,
-		},
-		{
-			name:               "fargate_config_feature_gate_enabled",
-			input:              "../../cmd/otelcol/config/collector/fargate_config.yaml",
-			expected:           "testdata/drop_signalfx_exporter_traces_pipelines/fargate_config_expected.yaml",
-			featureGateEnabled: true,
-		},
-		{
-			name:               "fargate_config_feature_gate_disabled",
-			input:              "../../cmd/otelcol/config/collector/fargate_config.yaml",
-			expected:           "../../cmd/otelcol/config/collector/fargate_config.yaml",
-			featureGateEnabled: false,
-		},
-		{
-			name:               "gateway_config_feature_gate_enabled",
-			input:              "../../cmd/otelcol/config/collector/gateway_config.yaml",
-			expected:           "testdata/drop_signalfx_exporter_traces_pipelines/gateway_config_expected.yaml",
-			featureGateEnabled: true,
-		},
-		{
-			name:               "gateway_config_feature_gate_disabled",
-			input:              "../../cmd/otelcol/config/collector/gateway_config.yaml",
-			expected:           "../../cmd/otelcol/config/collector/gateway_config.yaml",
-			featureGateEnabled: false,
-		},
-		{
-			name:               "upstream_agent_config_feature_gate_enabled",
-			input:              "../../cmd/otelcol/config/collector/upstream_agent_config.yaml",
-			expected:           "testdata/drop_signalfx_exporter_traces_pipelines/upstream_agent_config_expected.yaml",
-			featureGateEnabled: true,
-		},
-		{
-			name:               "upstream_agent_config_feature_gate_disabled",
-			input:              "../../cmd/otelcol/config/collector/upstream_agent_config.yaml",
-			expected:           "../../cmd/otelcol/config/collector/upstream_agent_config.yaml",
-			featureGateEnabled: false,
-		},
-		{
-			name:               "full_config_linux_feature_gate_enabled",
-			input:              "../../cmd/otelcol/config/collector/full_config_linux.yaml",
-			expected:           "testdata/drop_signalfx_exporter_traces_pipelines/full_config_linux_expected.yaml",
-			featureGateEnabled: true,
-		},
-		{
-			name:               "full_config_linux_feature_gate_disabled",
-			input:              "../../cmd/otelcol/config/collector/full_config_linux.yaml",
-			expected:           "../../cmd/otelcol/config/collector/full_config_linux.yaml",
-			featureGateEnabled: false,
-		},
-		{
-			name:               "logs_config_linux_feature_gate_enabled",
-			input:              "../../cmd/otelcol/config/collector/logs_config_linux.yaml",
-			expected:           "testdata/drop_signalfx_exporter_traces_pipelines/logs_config_linux_expected.yaml",
-			featureGateEnabled: true,
-		},
-		{
-			name:               "logs_config_linux_feature_gate_disabled",
-			input:              "../../cmd/otelcol/config/collector/logs_config_linux.yaml",
-			expected:           "../../cmd/otelcol/config/collector/logs_config_linux.yaml",
-			featureGateEnabled: false,
-		},
-		{
-			name:               "otlp_config_linux_feature_gate_enabled",
-			input:              "../../cmd/otelcol/config/collector/otlp_config_linux.yaml",
-			expected:           "testdata/drop_signalfx_exporter_traces_pipelines/otlp_config_linux_expected.yaml",
-			featureGateEnabled: true,
-		},
-		{
-			name:               "otlp_config_linux_feature_gate_disabled",
-			input:              "../../cmd/otelcol/config/collector/otlp_config_linux.yaml",
-			expected:           "../../cmd/otelcol/config/collector/otlp_config_linux.yaml",
-			featureGateEnabled: false,
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			require.NoError(t, featuregate.GlobalRegistry().Set(dropTraceCorrelationPipelineFeatureGateID, tt.featureGateEnabled))
-
-			cfgMap, err := confmaptest.LoadConf(tt.input)
-			require.NoError(t, err)
-			require.NotNil(t, cfgMap)
-
-			expectedCfgMap, err := confmaptest.LoadConf(tt.expected)
-			require.NoError(t, err)
-			require.NotNil(t, expectedCfgMap)
-
-			require.NoError(t, DropSignalFxTracesExporterIfFeatureGateEnabled(t.Context(), cfgMap))
-
-			require.Equal(t, expectedCfgMap, cfgMap)
-		})
-	}
-}
-
-func TestIsTracePipeline(t *testing.T) {
-	tests := []struct {
-		name     string
-		pipeline string
-		expected bool
-	}{
-		{
-			name:     "traces_pipeline",
-			pipeline: "traces",
-			expected: true,
-		},
-		{
-			name:     "alternate_traces_pipeline",
-			pipeline: "traces/alternate",
-			expected: true,
-		},
-		{
-			name:     "metrics_pipeline",
-			pipeline: "metrics",
-			expected: false,
-		},
-		{
-			name:     "internal_metrics_pipeline",
-			pipeline: "metrics/internal",
-			expected: false,
-		},
-		{
-			name:     "logs_pipeline",
-			pipeline: "logs",
-			expected: false,
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			require.Equal(t, tt.expected, isTracePipeline(tt.pipeline))
-		})
-	}
-}
-
-func TestIsSignalFxExporter(t *testing.T) {
-	tests := []struct {
-		name     string
-		exporter string
-		expected bool
-	}{
-		{
-			name:     "default_signalfx_exporter",
-			exporter: "signalfx",
-			expected: true,
-		},
-		{
-			name:     "alternate_signalfx_exporter",
-			exporter: "signalfx/alternate",
-			expected: true,
-		},
-		{
-			name:     "otlp_http_exporter",
-			exporter: "otlp_http",
-			expected: false,
-		},
-		{
-			name:     "splunk_hec_exporter",
-			exporter: "splunk_hec",
-			expected: false,
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			require.Equal(t, tt.expected, isSignalFxExporter(tt.exporter))
-		})
-	}
-}
-
 func TestNoOpConfigs(t *testing.T) {
 	tests := []struct {
 		config        *confmap.Conf
@@ -319,6 +108,166 @@ func TestNoOpConfigs(t *testing.T) {
 				require.NoError(t, err)
 			}
 			require.Equal(t, original, config)
+		})
+	}
+}
+
+func TestDropSignalFxTracesExporterIfFeatureGateEnabled(t *testing.T) {
+	tests := []struct {
+		name     string
+		input    string
+		expected string
+	}{
+		{
+			name:     "drop_signalfx_exporter_traces_pipelines",
+			input:    "testdata/drop_signalfx_exporter_traces_pipelines/input_config.yaml",
+			expected: "testdata/drop_signalfx_exporter_traces_pipelines/expected_config.yaml",
+		},
+		{
+			name:     "drop_traces_pipelines",
+			input:    "testdata/drop_signalfx_exporter_traces_pipelines/drop_traces_pipeline_input.yaml",
+			expected: "testdata/drop_signalfx_exporter_traces_pipelines/drop_traces_pipeline_expected.yaml",
+		},
+		{
+			name:     "agent_config",
+			input:    "../../cmd/otelcol/config/collector/agent_config.yaml",
+			expected: "testdata/drop_signalfx_exporter_traces_pipelines/agent_config_expected.yaml",
+		},
+		{
+			name:     "ecs_ec2_config",
+			input:    "../../cmd/otelcol/config/collector/ecs_ec2_config.yaml",
+			expected: "testdata/drop_signalfx_exporter_traces_pipelines/ecs_ec2_config_expected.yaml",
+		},
+		{
+			name:     "fargate_config",
+			input:    "../../cmd/otelcol/config/collector/fargate_config.yaml",
+			expected: "testdata/drop_signalfx_exporter_traces_pipelines/fargate_config_expected.yaml",
+		},
+		{
+			name:     "gateway_config",
+			input:    "../../cmd/otelcol/config/collector/gateway_config.yaml",
+			expected: "testdata/drop_signalfx_exporter_traces_pipelines/gateway_config_expected.yaml",
+		},
+		{
+			name:     "upstream_agent_config",
+			input:    "../../cmd/otelcol/config/collector/upstream_agent_config.yaml",
+			expected: "testdata/drop_signalfx_exporter_traces_pipelines/upstream_agent_config_expected.yaml",
+		},
+		{
+			name:     "full_config_linux",
+			input:    "../../cmd/otelcol/config/collector/full_config_linux.yaml",
+			expected: "testdata/drop_signalfx_exporter_traces_pipelines/full_config_linux_expected.yaml",
+		},
+		{
+			name:     "logs_config_linux",
+			input:    "../../cmd/otelcol/config/collector/logs_config_linux.yaml",
+			expected: "testdata/drop_signalfx_exporter_traces_pipelines/logs_config_linux_expected.yaml",
+		},
+		{
+			name:     "otlp_config_linux",
+			input:    "../../cmd/otelcol/config/collector/otlp_config_linux.yaml",
+			expected: "testdata/drop_signalfx_exporter_traces_pipelines/otlp_config_linux_expected.yaml",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			inputCfgMap, err := confmaptest.LoadConf(tt.input)
+			require.NoError(t, err)
+			require.NotNil(t, inputCfgMap)
+
+			require.NoError(t, featuregate.GlobalRegistry().Set(dropTraceCorrelationPipelineFeatureGateID, false))
+			disabledCfgMap, err := confmaptest.LoadConf(tt.input)
+			require.NoError(t, err)
+			require.NotNil(t, disabledCfgMap)
+			require.NoError(t, DropSignalFxTracesExporterIfFeatureGateEnabled(t.Context(), disabledCfgMap))
+			require.Equal(t, inputCfgMap, disabledCfgMap)
+
+			expectedCfgMap, err := confmaptest.LoadConf(tt.expected)
+			require.NoError(t, err)
+			require.NotNil(t, expectedCfgMap)
+
+			require.NoError(t, featuregate.GlobalRegistry().Set(dropTraceCorrelationPipelineFeatureGateID, true))
+			enabledCfgMap, err := confmaptest.LoadConf(tt.input)
+			require.NoError(t, err)
+			require.NotNil(t, enabledCfgMap)
+			require.NoError(t, DropSignalFxTracesExporterIfFeatureGateEnabled(t.Context(), enabledCfgMap))
+			require.Equal(t, expectedCfgMap, enabledCfgMap)
+		})
+	}
+}
+
+func TestIsTracePipeline(t *testing.T) {
+	tests := []struct {
+		name     string
+		pipeline string
+		expected bool
+	}{
+		{
+			name:     "traces_pipeline",
+			pipeline: "traces",
+			expected: true,
+		},
+		{
+			name:     "alternate_traces_pipeline",
+			pipeline: "traces/alternate",
+			expected: true,
+		},
+		{
+			name:     "metrics_pipeline",
+			pipeline: "metrics",
+			expected: false,
+		},
+		{
+			name:     "internal_metrics_pipeline",
+			pipeline: "metrics/internal",
+			expected: false,
+		},
+		{
+			name:     "logs_pipeline",
+			pipeline: "logs",
+			expected: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			require.Equal(t, tt.expected, isTracePipeline(tt.pipeline))
+		})
+	}
+}
+
+func TestIsSignalFxExporter(t *testing.T) {
+	tests := []struct {
+		name     string
+		exporter string
+		expected bool
+	}{
+		{
+			name:     "default_signalfx_exporter",
+			exporter: "signalfx",
+			expected: true,
+		},
+		{
+			name:     "alternate_signalfx_exporter",
+			exporter: "signalfx/alternate",
+			expected: true,
+		},
+		{
+			name:     "otlp_http_exporter",
+			exporter: "otlp_http",
+			expected: false,
+		},
+		{
+			name:     "splunk_hec_exporter",
+			exporter: "splunk_hec",
+			expected: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			require.Equal(t, tt.expected, isSignalFxExporter(tt.exporter))
 		})
 	}
 }

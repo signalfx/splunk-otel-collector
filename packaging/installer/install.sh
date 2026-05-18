397516c949f50813bda699ae56a85fe4e8353043
Olga <86965961+omrozowicz-splunk@users.noreply.github.com>
2026-05-18 12:50:13 +0200

    fix most of the PR's comments

--- packaging/installer/install.sh
+++ packaging/installer/install.sh
@@ -1945,8 +1945,8 @@ parse_args_and_install() {
     mkdir -p "$logs_file_storage_path"
     chown -R $service_user:$service_group "$logs_file_storage_path"
     chmod 700 "$logs_file_storage_path"
-      configure_env_file "SPLUNK_PLATFORM_URL" "$splunk_platform_url" "$collector_env_path"
-      configure_env_file "SPLUNK_PLATFORM_TOKEN" "$splunk_platform_token" "$collector_env_path"
+    configure_env_file "SPLUNK_PLATFORM_URL" "$splunk_platform_url" "$collector_env_path"
+    configure_env_file "SPLUNK_PLATFORM_TOKEN" "$splunk_platform_token" "$collector_env_path"
     configure_env_file "SPLUNK_FILE_STORAGE_EXTENSION_PATH" "$logs_file_storage_path" "$collector_env_path"
     if [ -n "$splunk_platform_logs_index" ]; then
       configure_env_file "SPLUNK_PLATFORM_LOGS_INDEX" "$splunk_platform_logs_index" "$collector_env_path"
@@ -1996,7 +1996,7 @@ EOH
 [NOTICE] Log collection has been enabled using $logs_config_path.
 
 Journald log collection is disabled by default. To enable it:
-  1. Uncomment the 'journald' receiver and 'logs/journald' pipeline in $logs_config_path.
+  1. Uncomment the 'journald' entry in the 'logs/hec' pipeline in $logs_config_path.
   2. Grant the collector service user permission to read the journal:
 
        sudo usermod -aG systemd-journal $service_user

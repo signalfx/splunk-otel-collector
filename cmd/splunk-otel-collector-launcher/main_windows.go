7d5900b3c527acd7f37fb8a2da1eb16036b826ac
Kelsey Ma <kelsey.ma6@gmail.com>
2026-06-02 11:55:50 -0700

    Add OpAMP Supervisor launcher core for remote configuration support (#7589)
    * Add OpAMP Supervisor launcher core for remote configuration support
    
    * Update linux default paths
    
    * address review comments, remove unnecessary checks and dir creation
    
    * create sanitized collector config instead of feature gates, ensure default listen interface behavior is carried over

---
+++ cmd/splunk-otel-collector-launcher/main_windows.go
@@ -0,0 +1,27 @@
+// Copyright Splunk Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//go:build windows
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/signalfx/splunk-otel-collector/internal/opampsupervisor/launcher"
+)
+
+func run(_, _ []string, _ launcher.Paths) error {
+	return fmt.Errorf("windows launcher service support is not implemented")
+}
---
+++ internal/opampsupervisor/launcher/paths_windows_test.go
@@ -0,0 +1,53 @@
+// Copyright Splunk Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//go:build windows
+
+package launcher
+
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDefaultPathsWindowsFallbacksAlignWithInstaller(t *testing.T) {
+	t.Setenv("ProgramFiles", "")
+	t.Setenv("ProgramData", "")
+
+	paths := DefaultPaths()
+
+	installDir := filepath.Join(`\Program Files`, "Splunk", "OpenTelemetry Collector")
+	stateDir := filepath.Join(`\ProgramData`, "Splunk", "OpenTelemetry Collector", "supervisor")
+
+	assert.Equal(t, filepath.Join(installDir, "otelcol.exe"), paths.CollectorExecutable)
+	assert.Equal(t, filepath.Join(installDir, "opampsupervisor.exe"), paths.SupervisorExecutable)
+	assert.Equal(t, filepath.Join(stateDir, "supervisor_config.yaml"), paths.SupervisorConfig)
+	assert.Equal(t, filepath.Join(stateDir, "collector_config.yaml"), paths.GeneratedCollectorConfig)
+	assert.Equal(t, stateDir, paths.StorageDirectory)
+	assert.Equal(t, filepath.Join(`\ProgramData`, "Splunk", "OpenTelemetry Collector", "agent_config.yaml"), paths.DefaultAgentConfig)
+	assert.False(t, paths.UseHUPConfigReload)
+}
+
+func TestDefaultListenInterfaceForWindowsAgentConfig(t *testing.T) {
+	t.Setenv("ProgramData", "")
+
+	programData := `\ProgramData`
+	agentConfig := filepath.Join(programData, "Splunk", "OpenTelemetry Collector", "agent_config.yaml")
+	gatewayConfig := filepath.Join(programData, "Splunk", "OpenTelemetry Collector", "gateway_config.yaml")
+
+	assert.Equal(t, defaultAgentListenInterface, defaultListenInterfaceForConfig(agentConfig, agentConfig))
+	assert.Equal(t, defaultListenInterface, defaultListenInterfaceForConfig(gatewayConfig, agentConfig))
+}

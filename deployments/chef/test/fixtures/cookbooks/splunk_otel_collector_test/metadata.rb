3f38b2be71a340b83e40df1f150151d404e3214a
Dmitry Anoshin <anoshindx@gmail.com>
2026-07-20 23:01:05 +0000

    [chef] Migrate nodejs cookbook test setup

---
+++ deployments/chef/test/fixtures/cookbooks/splunk_otel_collector_test/metadata.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+name 'splunk_otel_collector_test'
+version '0.1.0'
+
+depends 'nodejs'
---
+++ deployments/chef/test/fixtures/cookbooks/splunk_otel_collector_test/recipes/nodejs.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+nodejs_install 'nodejs' do
+  install_method 'binary'
+  version '18.20.8'
+  # Required for idempotence.
+  append_env_path false
+  binary_checksums(
+    'linux_x64' => '27a9f3f14d5e99ad05a07ed3524ba3ee92f8ff8b6db5ff80b00f9feb5ec8097a'
+  )
+end

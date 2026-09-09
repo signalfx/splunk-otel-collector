de1beb3a9942ad28f53578f25357caf007bc0254
Paulo Janotti <pjanotti@splunk.com>
2026-09-09 16:27:19 -0700

    Remove debug logging from tests

--- packaging/tests/instrumentation/instrumentation_test.py
+++ packaging/tests/instrumentation/instrumentation_test.py
@@ -397,8 +397,6 @@ def test_express_instrumentation(distro, arch):
         # install splunk-otel-js to /usr/lib/splunk-instrumentation/splunk-otel-js
         run_container_cmd(container, f"mkdir -p {LIB_DIR}/splunk-otel-js")
         run_container_cmd(container, f"bash -l -c 'cd {LIB_DIR}/splunk-otel-js && npm install {NODE_AGENT_PATH}'")
-        run_container_cmd(container, "bash -c 'find / -name injector.conf'", user='root')
-        run_container_cmd(container, "bash -c 'find /usr/lib/splunk-instrumentation'", user='root')
 
         # attributes from default config
         attributes = {
